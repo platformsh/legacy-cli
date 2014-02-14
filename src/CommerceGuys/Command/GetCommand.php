cb134f73a517cf03910df156560d30afaf708706
Bojan Zivanovic <bojanz@gmail.com>
2014-02-14 01:11:49 +0100

    Add commands for listing and cloning projects.

---
+++ src/CommerceGuys/Command/GetCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace CommerceGuys\Command;
+
+use Guzzle\Http\ClientInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Dumper;
+
+class GetCommand extends PlatformCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('get')
+            ->setDescription('Does a git clone of the referenced project.')
+            ->addArgument(
+                'id',
+                InputArgument::OPTIONAL,
+                'The platform id'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->hasConfiguration()) {
+            $output->writeln("<error>Platform settings not initialized. Please run 'platform init'.</error>");
+            return;
+        }
+
+        $platformId = $input->getArgument('id');
+        if (empty($platformId)) {
+            $output->writeln("<error>You must provide a platform id.</error>");
+            return;
+        }
+
+        $client = $this->getClient('accounts');
+        $data = $client->getProjects();
+        $projects = array();
+        foreach ($data['projects'] as $project) {
+            $id = preg_replace('/[^a-z0-9-]+/i', '-', strtolower($project['name']));
+            $projects[$id] = $project;
+        }
+        if (!isset($projects[$platformId])) {
+            $output->writeln("<error>Platform id not found.</error>");
+            return;
+        }
+
+        $project = $projects[$platformId];
+        $uriParts = explode('/', str_replace('http://', '', $project['uri']));
+        $cluster = $uriParts[0];
+        $machineName = end($uriParts);
+
+        $gitUrl = "{$machineName}@git.{$cluster}:{$machineName}.git";
+        $command = "git clone " . $gitUrl;
+        passthru($command);
+    }
+}
---
+++ src/CommerceGuys/Command/ProjectListCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CommerceGuys\Command;
+
+use Guzzle\Http\ClientInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Dumper;
+
+class ProjectListCommand extends PlatformCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('project:list')
+            ->setDescription('Get a list of all active projects.');
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->hasConfiguration()) {
+            $output->writeln("<error>Platform settings not initialized. Please run 'platform init'.</error>");
+            return;
+        }
+
+        $client = $this->getClient('accounts');
+        $data = $client->getProjects();
+        $project_rows = array();
+        foreach ($data['projects'] as $project) {
+            $project_row = array();
+            $project_row[] = preg_replace('/[^a-z0-9-]+/i', '-', strtolower($project['name']));
+            $project_row[] = $project['name'];
+            $project_row[] = $project['uri'];
+            $project_rows[] = $project_row;
+        }
+
+        $table = $this->getHelperSet()->get('table');
+        $table
+            ->setHeaders(array('ID', 'Name', "URL"))
+            ->setRows($project_rows);
+        $table->render($output);
+        $output->writeln("\nYou can delete any project by running <info>platform project:delete [id]</info>.\n");
+    }
+}
