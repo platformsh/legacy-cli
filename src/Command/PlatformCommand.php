2162f9f2376e273d6371def2c2cd662f53ec9696
Bojan Zivanovic <bojanz@gmail.com>
2014-02-26 12:15:28 +0100

    Revert "Fix displaying projects with the same name."
    This reverts commit 92e4a4a614abf936bf64457699b52b610a3a43a9.

--- src/Command/PlatformCommand.php
+++ src/Command/PlatformCommand.php
@@ -162,7 +162,7 @@ class PlatformCommand extends Command
             foreach ($data['projects'] as $project) {
                 $machineName = preg_replace('/[^a-z0-9-]+/i', '-', strtolower($project['name']));
                 $project['machine_name'] = $machineName;
-                $projects[] = $project;
+                $projects[$machineName] = $project;
             }
             $this->config['projects'] = $projects;
         }
