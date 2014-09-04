99b80d2e02ba35d3a5f7bdd4c3cbc0b3f3e71b5d
Administrator <ori@pekelman.com>
2014-09-04 20:34:31 +0200

    make tunnels abstract

--- src/Command/EnvironmentTunnelCommand.php
+++ src/Command/EnvironmentTunnelCommand.php
@@ -96,18 +96,56 @@ class EnvironmentTunnelCommand extends EnvironmentCommand
             $message .= "</info>";
             return $message;
     }
-            
+
+    protected function create_procfile(){
+
+    }
+
+    /*FIXME this is hardcoded for the moment as the API does not expose the relations*/
+    protected function tunnels_config(){
+      $this->tunnels=[];
+      $relations_json='{"solr": [{"host": "solr.internal", "scheme": "solr", "port": 8080}], "redis": [{"host": "redis.internal", "scheme": "redis", "port": 6379}], "database": [{"username": "", "password": "", "host": "database.internal", "query": {"is_master": true}, "path": "main", "scheme": "mysql", "port": 3306}]}';
+      $port_delta = 10000;             // FIXME bad idea
+      $this->relations=json_decode($relations_json);
+      foreach ($this->relations as $relation => $config) {
+        $this->tunnels[$relation]=
+          array(
+            "host"=>$config[0]->host,
+            "port"=>$config[0]->port,
+            "local_port"=> (int)$config[0]->port + $port_delta
+          );
+      }
+    }
+    
+    protected function tunnels_params(){
+      $params=[];
+      foreach ($this->tunnels as $tunnel=>$config){
+        $host = $config['host'];
+        $port = $config['port'];
+        $local_port=$config['local_port'];
+        $params[]= "-L $local_port:$host:$port";
+      }
+      return join(" ",$params);
+    }
+    protected function web_params($buildpack, $projectWWWRoot, $localhost, $http_local_port, $projectWWWRoot){
+      switch ($buildpack){
+        case "drupal":
+        default:
+        return "php -t $projectWWWRoot -S $localhost:$http_local_port -r'if (preg_match(\"/\.(engine|inc|info|install|make|module|profile|test|po|sh|.*sql|theme|tpl(\.php)?|xtmpl)/\",\$_SERVER[\"REQUEST_URI\"])) { print \"Error\\n\"; } else if (preg_match(\"/(^|\/)\./\",\$_SERVER[\"REQUEST_URI\"])) { return false; } else if (file_exists(\$_SERVER[\"DOCUMENT_ROOT\"].\$_SERVER[\"SCRIPT_NAME\"])) { return false; } else {\$_GET[\"q\"]=\$_SERVER[\"REQUEST_URI\"]; include(\"$projectWWWRoot/index.php\"); }'";
+    }
+    }
+    
     protected function open_tunnel(){
+            $this->tunnels_config();
             $environmentId = $this->environment['id'];
             $projectId = $this->project['id'];
-            list($protocol,$sshUrl) = split('://',$this->environment["_links"]["ssh"]["href"]); // We can't pass the ssh:// protocol indentifier to the ssh command
+            list($protocol,$sshUrl) = split('://', $this->environment["_links"]["ssh"]["href"]); // We can't pass the ssh:// protocol indentifier to the ssh command
             $projectWWWRoot = $this->getProjectRoot()."/www";
             $localhost = "local.platform.sh";
-            $mysql_local_port= 33306;
-            $http_local_port=  8000;
+            $http_local_port=  8000; //FIXME hardcoded
             $tmp_dir="/tmp";
-            $tunnelCommand = "ssh -N -L$mysql_local_port:database.internal:3306 $sshUrl";
-            $serveCommand = "php -t $projectWWWRoot -S $localhost:$http_local_port -r'if (preg_match(\"/\.(engine|inc|info|install|make|module|profile|test|po|sh|.*sql|theme|tpl(\.php)?|xtmpl)/\",\$_SERVER[\"REQUEST_URI\"])) { print \"Error\\n\"; } else if (preg_match(\"/(^|\/)\./\",\$_SERVER[\"REQUEST_URI\"])) { return false; } else if (file_exists(\$_SERVER[\"DOCUMENT_ROOT\"].\$_SERVER[\"SCRIPT_NAME\"])) { return false; } else {\$_GET[\"q\"]=\$_SERVER[\"REQUEST_URI\"]; include(\"$projectWWWRoot/index.php\"); }'";
+            $tunnelCommand = "ssh -N ". $this->tunnels_params() ." $sshUrl";
+            $serveCommand = $this->web_params();
             $webOutputFile ="$tmp_dir/platform-local-web-server-$environmentId.log";
             $webPidFile ="$tmp_dir/platform-local-web-server-$environmentId.pid";
             $tunnelOutputFile ="$tmp_dir/platform-local-ssh-tunnel-$environmentId.log";
@@ -123,7 +161,10 @@ class EnvironmentTunnelCommand extends EnvironmentCommand
             $message = '<info>';
             if(!empty($killOutPut)){$message .= "Killing existing tunnel $killOutPut\n";}
             $message .= "\nA host has been configured as $localhost\n";
-            $message .= "\nA tunnel to environment $environmentId has been created. \n";
+            $message .= "\nA tunnel to environment $environmentId has been created ($tunnelCommand)\n";
+            foreach ($this->tunnels as $tunnel=>$config){
+              $message .= "Connect to $tunnel through $localhost:".$config["local_port"];
+            }
             $message .= "\nSet your settings to connect $localhost:$mysql_local_port for mysql (do not use localhost !)\n";
             $message .= "\nYou can see the site on http://$localhost:$http_local_port";
             $message .= "</info>";
