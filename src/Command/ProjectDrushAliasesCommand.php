cf17e96f9ab490c13b4f9ad7407c3a3f218f2cf2
Patrick Dawkins <pjcdawkins@gmail.com>
2014-09-12 11:11:51 +0100

    Add 'recreate' and 'pipe' options to drush-aliases, with fancier output.

--- src/Command/ProjectDrushAliasesCommand.php
+++ src/Command/ProjectDrushAliasesCommand.php
@@ -5,6 +5,8 @@ namespace CommerceGuys\Platform\Cli\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class ProjectDrushAliasesCommand extends PlatformCommand
 {
@@ -14,8 +16,10 @@ class ProjectDrushAliasesCommand extends PlatformCommand
         $this
             ->setName('project:drush-aliases')
             ->setAliases(array('drush-aliases'))
-            ->addOption('group', 'g', InputOption::VALUE_OPTIONAL, 'The alias group name.')
-            ->setDescription('Recreate the project\'s Drush aliases (if any).');
+            ->addOption('recreate', 'r', InputOption::VALUE_NONE, 'Recreate the aliases.')
+            ->addOption('group', 'g', InputOption::VALUE_OPTIONAL, 'Recreate the aliases with a new group name.')
+            ->addOption('pipe', 'p', InputOption::VALUE_NONE, 'Output the current group name (do nothing else).')
+            ->setDescription('Determine and/or recreate the project\'s Drush aliases (if any).');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -25,19 +29,27 @@ class ProjectDrushAliasesCommand extends PlatformCommand
             throw new \Exception('This can only be run from inside a project directory');
         }
 
+        $this->ensureDrushInstalled();
+
         $current_group = isset($project['alias-group']) ? $project['alias-group'] : $project['id'];
-        $new_group = $input->getOption('group');
-        $new_group = ltrim($new_group, '@');
+
+        if ($input->getOption('pipe')) {
+            $output->writeln($current_group);
+            return;
+        }
+
+        $new_group = ltrim($input->getOption('group'), '@');
 
         $this->loadConfig();
         $environments = $this->config['environments'][$project['id']];
 
         if ($new_group && $new_group != $current_group) {
-            $dialog = $this->getHelperSet()->get('dialog');
-            $existing = shell_exec("drush site-alias --pipe @" . escapeshellarg($new_group) . " 2>/dev/null");
+            $questionHelper = $this->getHelper('question');
+            $consoleOutput = new ConsoleOutput();
+            $existing = shell_exec("drush site-alias --pipe --format=list @" . escapeshellarg($new_group) . " 2>/dev/null");
             if ($existing) {
-                $question = "The alias group @$new_group already exists. Overwrite? [y/N] ";
-                if (!$dialog->askConfirmation($output, $question, false)) {
+                $question = new ConfirmationQuestion("The alias group @$new_group already exists. Overwrite? [y/N] ", false);
+                if (!$questionHelper->ask($input, $consoleOutput, $question)) {
                     return;
                 }
             }
@@ -49,15 +61,28 @@ class ProjectDrushAliasesCommand extends PlatformCommand
             $drushDir = $this->getApplication()->getHomeDirectory() . '/.drush';
             $oldFile = $drushDir . '/' . $current_group . '.aliases.drushrc.php';
             if (file_exists($oldFile)) {
-                $question = "Delete old alias group @$current_group? [y/N] ";
-                if ($dialog->askConfirmation($output, $question, false)) {
+                $question = new ConfirmationQuestion("Delete old alias group @$current_group? [Y/n] ");
+                if ($questionHelper->ask($input, $consoleOutput, $question)) {
                     unlink($oldFile);
                 }
             }
+
+            $current_group = $new_group;
         }
-        else {
+        elseif ($input->getOption('recreate')) {
             $this->createDrushAliases($project, $environments);
-            $output->writeln("Project aliases recreated, group: @$current_group");
+            $output->writeln("Project aliases recreated");
+        }
+
+        // Don't run expensive drush calls if they are not needed.
+        if ($input->getOption('quiet')) {
+            return;
+        }
+
+        $output->writeln("Aliases for <info>{$project['name']}</info> ({$project['id']}):");
+        $aliases = shell_exec("drush site-alias --pipe --format=list @" . escapeshellarg($current_group) . " 2>/dev/null");
+        foreach (explode("\n", $aliases) as $alias) {
+            $output->writeln('    @' . $alias);
         }
     }
 }
