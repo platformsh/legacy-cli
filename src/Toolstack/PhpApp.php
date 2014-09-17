ed634d3594f5d5f5b07f22c2b02d8ba898051b49
Christian Sieber <kazanir@gmail.com>
2014-09-17 10:36:52 +0200

    Initial toolstack scaffolding

---
+++ src/Toolstack/PhpApp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CommerceGuys\Platform\Cli\Toolstack;
+
+use CommerceGuys\Platform\Cli;
+use Symfony\Component\Console;
+
+class PhpApp extends BaseApp implements LocalBuildInterface
+{
+
+}
---
+++ src/Toolstack/SymfonyApp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CommerceGuys\Platform\Cli\Toolstack;
+
+use CommerceGuys\Platform\Cli;
+use Symfony\Component\Console;
+
+class SymfonyApp extends PhpApp implements LocalBuildInterface
+{
+
+}
