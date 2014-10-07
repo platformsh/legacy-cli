7fe5ad6b2aace49c231e86b500f880cdb46a48e2
Patrick Dawkins <pjcdawkins@gmail.com>
2014-10-07 22:36:06 +0100

    Improve checkout command usability.

--- src/Command/EnvironmentCheckoutCommand.php
+++ src/Command/EnvironmentCheckoutCommand.php
@@ -4,8 +4,8 @@ namespace CommerceGuys\Platform\Cli\Command;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class EnvironmentCheckoutCommand extends EnvironmentCommand
 {
@@ -15,43 +15,64 @@ class EnvironmentCheckoutCommand extends EnvironmentCommand
         $this
             ->setName('environment:checkout')
             ->setAliases(array('checkout'))
-            ->setDescription('Checkout an environment.')
+            ->setDescription('Check out an environment.')
             ->addArgument(
                 'branch-name',
                 InputArgument::OPTIONAL,
-                'The name of the branch to checkout. For example: "sprint2"'
-            )
-            ->addOption(
-                'project',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'The project id'
-            )
-            ->addOption(
-                'environment',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'The environment id'
+                'The name of the branch to check out. For example: "sprint2"'
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!$this->validateInput($input, $output)) {
-            return;
-        }
-        $branch = $input->getArgument('branch-name');
-        if (empty($branch)) {
-            $output->writeln("<error>You must specify the name of the branch to checkout.</error>");
-            return;
-        }
-
-        // Checkout the new branch locally.
-        $projectRoot = $this->getProjectRoot();
-        if (!$projectRoot) {
+        $project = $this->getCurrentProject();
+        if (!$project) {
             throw new \Exception('This can only be run from inside a project directory');
         }
-        $repositoryDir = $projectRoot . '/repository';
-        passthru("cd " . escapeshellarg($repositoryDir) . " && git fetch origin && git checkout $branch");
+
+        $branch = $input->getArgument('branch-name');
+        if (empty($branch) && $input->isInteractive()) {
+            $environments = $this->getEnvironments($project);
+            $environmentList = array();
+            foreach ($environments as $environment) {
+                $environmentList[] = $environment['id'];
+            }
+            $chooseEnvironmentText = "Enter a number to choose which environment to check out:";
+            $helper = $this->getHelper('question');
+            $question = new ChoiceQuestion($chooseEnvironmentText, $environmentList);
+            $question->setMaxAttempts(5);
+            $machineName = $helper->ask($input, $output, $question);
+        }
+        elseif (empty($branch)) {
+            $output->writeln("<error>No branch specified.</error>");
+            return 1;
+        }
+        else {
+            $machineName = $this->sanitizeEnvironmentId($branch);
+        }
+
+        $projectRoot = $this->getProjectRoot();
+
+        chdir($projectRoot . '/repository');
+
+        $existsLocal = $this->shellExec("git show-ref refs/heads/$machineName");
+
+        // If the branch doesn't already exist locally, check whether it is a
+        // Platform.sh environment.
+        if (!$existsLocal) {
+            if (!isset($environments)) {
+                $environments = $this->getEnvironments($project);
+            }
+            if (!isset($environments[$machineName])) {
+                $output->writeln("<error>Environment not found: $machineName</error>");
+                return 1;
+            }
+            // Fetch from origin.
+            // @todo don't assume that the Platform.sh remote is called 'origin'
+            passthru('git fetch origin');
+        }
+
+        // Check out the branch.
+        passthru("git checkout $machineName");
     }
 }
