12a6b752cafb0d2f5dd68d825f74150f647eedbe
Patrick Dawkins <pjcdawkins@gmail.com>
2014-10-11 16:09:53 +0100

    Allow alternative provider and show curl errors for env:ip

--- src/Command/EnvironmentIpCommand.php
+++ src/Command/EnvironmentIpCommand.php
@@ -17,6 +17,7 @@ class EnvironmentIpCommand extends EnvironmentCommand
             ->setName('environment:ip')
             ->addOption('project', null, InputOption::VALUE_OPTIONAL, 'The project id')
             ->addOption('environment', null, InputOption::VALUE_OPTIONAL, 'The environment id')
+            ->addOption('provider', null, InputOption::VALUE_OPTIONAL, 'The IP address provider URL.', 'https://icanhazip.com/')
             ->addOption('pipe', null, InputOption::VALUE_NONE, 'Output the raw IP address, suitable for piping to another command.')
             ->setDescription('Get the public IP address of an environment.');
     }
@@ -27,9 +28,17 @@ class EnvironmentIpCommand extends EnvironmentCommand
             return 1;
         }
 
+        $provider = $input->getOption('provider');
+        if (!filter_var($provider, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED & FILTER_FLAG_SCHEME_REQUIRED)) {
+            $output->writeln('<error>Invalid provider URL.</error>');
+            return 1;
+        }
+
         $sshUrlString = $this->getSshUrl();
 
-        $ip = trim(shell_exec('ssh ' . $sshUrlString . ' "curl --silent --max-time 30 https://icanhazip.com/"'));
+        $curlCommand = "curl --silent --show-error --max-time 30 $provider";
+
+        $ip = trim(shell_exec("ssh $sshUrlString " . escapeshellarg($curlCommand)));
         if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             $output->writeln('<error>The IP address could not be found.</error>');
             return 1;
