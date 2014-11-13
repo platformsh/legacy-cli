6e4727f9b41d68b222ff3ca4e8863e0eb805eaf0
Patrick Dawkins <pjcdawkins@gmail.com>
2014-11-13 09:37:54 +0000

    Test shell with a command that will work on Windows too.

--- tests/Helper/ShellHelperTest.php
+++ tests/Helper/ShellHelperTest.php
@@ -20,7 +20,7 @@ class ShellHelperTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute()
     {
-        $this->assertNotEmpty($this->shellHelper->execute('which git'));
+        $this->assertNotEmpty($this->shellHelper->execute('help'));
         $this->assertEmpty($this->shellHelper->execute('which nonexistent'));
         $this->assertEmpty($this->shellHelper->execute('nonexistent command test'));
     }
@@ -30,10 +30,10 @@ class ShellHelperTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecuteArgs()
     {
-        $this->assertNotEmpty($this->shellHelper->executeArgs(array('which', 'git')), false);
+        $this->assertNotEmpty($this->shellHelper->executeArgs(array('help')), false);
         $this->assertEmpty($this->shellHelper->executeArgs(array('which', 'nonexistent'), false));
 
-        $this->assertNotEmpty($this->shellHelper->executeArgs(array('which', 'git')), true);
+        $this->assertNotEmpty($this->shellHelper->executeArgs(array('help')), true);
         $this->setExpectedException('Symfony\\Component\\Process\\Exception\\ProcessFailedException');
         $this->shellHelper->executeArgs(array('which', 'nonexistent'), true);
     }
