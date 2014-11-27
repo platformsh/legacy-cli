30f1b8d18ed37b3fa9866edc101da60cba26430e
Patrick Dawkins <pjcdawkins@gmail.com>
2014-11-27 08:01:01 +0000

    Test some of HalResource.

---
+++ tests/Model/HalResourceTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace CommerceGuys\Platform\Cli\Tests;
+
+use CommerceGuys\Platform\Cli\Model\HalResource;
+
+class HalResourceTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @var array */
+    protected $properties;
+
+    /** @var HalResource */
+    protected $resource;
+
+    public function setUp()
+    {
+        $this->properties = array(
+          'id' => 'test-id',
+          'name' => 'test name',
+          'array' => array(),
+          'integer' => 123,
+        );
+        $data = $this->properties + array(
+            '_embedded' => array(),
+            '_links' => array(
+                '#operate' => array(
+                  'href' => 'https://example.com/',
+                ),
+            ),
+          );
+        $this->resource = new HalResource($data);
+    }
+
+    /**
+     * Test HalResource::getProperties().
+     */
+    public function testGetProperties()
+    {
+        $this->assertEquals(array_keys($this->properties), array_values($this->resource->getPropertyNames()));
+        $this->assertEquals($this->properties, $this->resource->getProperties());
+    }
+
+    /**
+     * Test HalResource::getProperty().
+     */
+    public function testGetProperty()
+    {
+        $this->assertEquals('test-id', $this->resource->id());
+        $this->assertEquals('test name', $this->resource->getProperty('name'));
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->resource->getProperty('nonexistent');
+    }
+
+    /**
+     * Test HalResource::operationAllowed().
+     */
+    public function testOperationAllowed()
+    {
+        $this->assertTrue($this->resource->operationAllowed('operate'));
+        $this->assertFalse($this->resource->operationAllowed('nonexistent'));
+    }
+
+}
