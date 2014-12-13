e80ae2cbea5428e5e601671ec2701549d0075514
Patrick Dawkins <pjcdawkins@gmail.com>
2014-12-13 17:51:38 +0000

    Remove out-of-date version from dev branch.

--- src/Application.php
+++ src/Application.php
@@ -28,7 +28,7 @@ class Application extends ParentApplication {
      */
     public function __construct()
     {
-        parent::__construct('Platform.sh CLI', '1.5-dev');
+        parent::__construct('Platform.sh CLI', '1.x-dev');
 
         $this->setDefaultTimezone();
 
