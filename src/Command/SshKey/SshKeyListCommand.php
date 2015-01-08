c4fe306c0ac699d659ac74f5cddcd89d7f1e04c0
Patrick Dawkins <pjcdawkins@gmail.com>
2015-01-08 08:16:02 +0000

    and use --format for ssh-keys

--- src/Command/SshKey/SshKeyListCommand.php
+++ src/Command/SshKey/SshKeyListCommand.php
@@ -2,7 +2,7 @@
 namespace Platformsh\Cli\Command\SshKey;
 
 use Platformsh\Cli\Command\CommandBase;
-use Symfony\Component\Console\Helper\Table;
+use Platformsh\Cli\Util\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -14,7 +14,8 @@ class SshKeyListCommand extends CommandBase
         $this
             ->setName('ssh-key:list')
             ->setAliases(['ssh-keys'])
-            ->setDescription('Get a list of SSH keys in your account');;
+            ->setDescription('Get a list of SSH keys in your account');
+        Table::addFormatOption($this->getDefinition());
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -25,21 +26,27 @@ class SshKeyListCommand extends CommandBase
         if (empty($keys)) {
             $this->stdErr->writeln("You do not yet have any SSH public keys in your Platform.sh account");
         } else {
-            $this->stdErr->writeln("Your SSH keys are:");
-            $table = new Table($output);
+            $table = new Table($input, $output);
             $headers = ['ID', 'Title', 'Fingerprint'];
             $rows = [];
             foreach ($keys as $key) {
                 $rows[] = [$key['key_id'], $key['title'], $key['fingerprint']];
             }
-            $table->setHeaders($headers);
-            $table->addRows($rows);
-            $table->render();
+            if ($table->formatIsMachineReadable()) {
+                $table->render($rows, $headers);
+
+                return 0;
+            }
+
+            $this->stdErr->writeln("Your SSH keys are:");
+            $table->render($rows, $headers);
         }
 
         $this->stdErr->writeln('');
 
         $this->stdErr->writeln("Add a new SSH key by running <info>platform ssh-key:add [path]</info>");
         $this->stdErr->writeln("Delete an SSH key by running <info>platform ssh-key:delete [id]</info>");
+
+        return !empty($keys) ? 0 : 1;
     }
 }
