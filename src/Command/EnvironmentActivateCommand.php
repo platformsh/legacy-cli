bbf49ece010619d2e1f8fe939075ca4f7174a907
Patrick Dawkins <pjcdawkins@gmail.com>
2015-01-09 12:07:37 +0000

    Allow a plural list of environments for the activate command.

--- src/Command/EnvironmentActivateCommand.php
+++ src/Command/EnvironmentActivateCommand.php
@@ -14,7 +14,7 @@ class EnvironmentActivateCommand extends EnvironmentCommand
         $this
             ->setName('environment:activate')
             ->setDescription('Activate an environment')
-            ->addArgument('environment', InputArgument::OPTIONAL, 'The environment to activate');
+            ->addArgument('environment', InputArgument::IS_ARRAY, 'The environment(s) to activate');
         $this->addProjectOption()->addEnvironmentOption();
     }
 
@@ -24,23 +24,64 @@ class EnvironmentActivateCommand extends EnvironmentCommand
             return 1;
         }
 
-        $environmentId = $this->environment['id'];
+        $environments = $this->environment ? array($this->environment) : $input->getArgument('environment');
 
-        if (!$this->operationAllowed('activate')) {
-            if (!empty($this->environment['_links']['public-url'])) {
-                $output->writeln("The environment <info>$environmentId</info> is already active.");
-                return 0;
-            }
-            $output->writeln(
-              "Operation not permitted: The environment <error>$environmentId</error> can't be activated."
-            );
-            return 1;
-        }
+        $success = $this->activateMultiple($environments, $input, $output);
 
-        $client = $this->getPlatformClient($this->environment['endpoint']);
-        $client->activateEnvironment();
-
-        $output->writeln("The environment <info>$environmentId</info> has been activated.");
-        return 0;
+        return $success ? 0 : 1;
     }
+
+    /**
+     * @param array           $environments
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return bool
+     */
+    protected function activateMultiple(array $environments, InputInterface $input, OutputInterface $output)
+    {
+        $count = count($environments);
+        $processed = 0;
+        // Confirm which environments the user wishes to be deactivated.
+        $process = array();
+        $questionHelper = $this->getHelper('question');
+        foreach ($environments as $environment) {
+            if (!is_array($environment)) {
+                $requested = $environment;
+                $environment = $this->getEnvironment($environment, $this->project);
+                if (!$environment) {
+                    $output->writeln("Environment not found: <error>$requested</error>");
+                    continue;
+                }
+            }
+            $environmentId = $environment['id'];
+            if (!empty($environment['_links']['public-url'])) {
+                $output->writeln("The environment <info>$environmentId</info> is already active.");
+                $processed++;
+                continue;
+            }
+            if (!$this->operationAllowed('activate', $environment)) {
+                $output->writeln("Operation not permitted: The environment <error>$environmentId</error> can't be activated.");
+                continue;
+            }
+            $question = "Are you sure you want to activate the environment <info>$environmentId</info>?";
+            if (!$questionHelper->confirm($question, $input, $output)) {
+                continue;
+            }
+            $process[$environmentId] = $environment;
+        }
+        foreach ($process as $environmentId =>  $environment) {
+            $client = $this->getPlatformClient($environment['endpoint']);
+            try {
+                $client->activateEnvironment();
+                $processed++;
+                $output->writeln("Activated environment <info>$environmentId</info>");
+            }
+            catch (\Exception $e) {
+                $output->writeln($e->getMessage());
+            }
+        }
+        return $processed >= $count;
+    }
+
 }
