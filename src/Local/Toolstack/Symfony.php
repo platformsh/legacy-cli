2c93b0a81692c9f83ba59d93483ca107c42052ab
Patrick Dawkins <pjcdawkins@gmail.com>
2015-02-16 16:45:19 +0000

    Remove symfony YAML files.

--- src/Local/Toolstack/Symfony.php
+++ src/Local/Toolstack/Symfony.php
@@ -11,29 +11,13 @@ class Symfony extends Composer
 
     public function detect($appRoot)
     {
-        if (file_exists($appRoot . '/composer.json')) {
-            $json = file_get_contents($appRoot . '/composer.json');
-            $composer_json = json_decode($json);
-            if (property_exists($composer_json->require, "symfony/symfony")) {
-              return TRUE; // @todo: Find a better way to test for Symfony. Some projects do not have this dep.
-            }
+        $composerJsonFile = "$appRoot/composer.json";
+        if (file_exists($composerJsonFile)) {
+            $composerJson = json_decode(file_get_contents($composerJsonFile), true);
+            return isset($composerJson['require']['symfony/symfony']);
         }
-        
-        return FALSE;
-    }
 
-    public function install()
-    {
-        $configDir = $this->buildDir . '/app/config';
-        $sharedDir = $this->getSharedDir();
-        $symfonyResources = CLI_ROOT . '/resources/symfony';
-
-        // Create and symlink configuration files.
-        // @todo are these actually useful?
-        foreach (array('config_dev.yml', 'routing_dev.yml') as $file) {
-            $this->fsHelper->copy("$symfonyResources/$file", "$sharedDir/$file");
-            $this->fsHelper->symLink("$sharedDir/$file", "$configDir/$file");
-        }
+        return false;
     }
 
 }
