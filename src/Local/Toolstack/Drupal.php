5729b10a614295ed02805ee7e66b3e36acb0b176
Patrick Dawkins <pjcdawkins@gmail.com>
2015-03-19 15:08:19 +0000

    Explicitly ignore settings.local.php, and make it obvious when symlinks conflict.
    Do not bother symlinking it into sites/default: it is never used in the
    Platform.sh build process.
    
    Conflicts:
    	src/Helper/FilesystemHelper.php

--- src/Local/Toolstack/Drupal.php
+++ src/Local/Toolstack/Drupal.php
@@ -158,6 +158,7 @@ class Drupal extends ToolstackBase
         $this->processSettingsPhp();
 
         $this->ignoredFiles[] = 'project.make';
+        $this->ignoredFiles[] = 'settings.local.php';
         $this->specialDestinations['sites.php'] = '{webroot}/sites';
 
         // Symlink, non-recursively, all files from the app into the
@@ -220,6 +221,7 @@ class Drupal extends ToolstackBase
 
         $this->ignoredFiles[] = basename($projectMake);
         $this->ignoredFiles[] = basename($projectCoreMake);
+        $this->ignoredFiles[] = 'settings.local.php';
 
         $this->specialDestinations['settings*.php'] = '{webroot}/sites/default';
         $this->specialDestinations['sites.php'] = '{webroot}/sites';
