f660519f17a84533d131bd34d9b1d8400e49490e
Patrick Dawkins <pjcdawkins@gmail.com>
2015-03-28 11:43:55 +0000

    Revert "Custom $config_directories break beta9"
    This reverts commit 56f8bdccb46b3b9a6ffe768235fece0341a63568.
    
    Pending answer to core issue: https://www.drupal.org/node/2461341

--- resources/drupal/8/settings.local.php
+++ resources/drupal/8/settings.local.php
@@ -14,12 +14,8 @@ $databases['default']['default'] = array(
 // You should modify this.
 $settings['hash_salt'] = '4946c1912834b8477cc70af309a2c30dcfc24c2103c724ff30bf13b4c10efd82';
 
-$config_directories = array();
-
-// Custom configuration directories.
-// Unfortunately, this appears to prevent installs of Drupal 8.0.0-beta9.
-//
-// $config_directories = array(
-//   CONFIG_ACTIVE_DIRECTORY => '../../config/active',
-//   CONFIG_STAGING_DIRECTORY => '../../config/staging',
-// );
+// Configuration directories.
+$config_directories = array(
+  CONFIG_ACTIVE_DIRECTORY => '../shared/config/active',
+  CONFIG_STAGING_DIRECTORY => '../shared/config/staging',
+);
