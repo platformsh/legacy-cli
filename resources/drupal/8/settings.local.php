48fc1c29a2515e0762666c07b742feab65341b19
Patrick Dawkins <pjcdawkins@gmail.com>
2015-03-30 11:26:30 +0100

    Drupal root is now 3 levels away from project root

--- resources/drupal/8/settings.local.php
+++ resources/drupal/8/settings.local.php
@@ -16,6 +16,6 @@ $settings['hash_salt'] = '4946c1912834b8477cc70af309a2c30dcfc24c2103c724ff30bf13
 
 // Configuration directories.
 $config_directories = array(
-  CONFIG_ACTIVE_DIRECTORY => '../../shared/config/active',
-  CONFIG_STAGING_DIRECTORY => '../../shared/config/staging',
+  CONFIG_ACTIVE_DIRECTORY => '../../../shared/config/active',
+  CONFIG_STAGING_DIRECTORY => '../../../shared/config/staging',
 );
