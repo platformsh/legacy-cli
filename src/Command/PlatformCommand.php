3969f2f599f465d3983886372d730fdf31238235
Patrick Dawkins <pjcdawkins@gmail.com>
2015-05-09 14:08:55 +0100

    Remove unused Yaml import

--- src/Command/PlatformCommand.php
+++ src/Command/PlatformCommand.php
@@ -17,7 +17,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\StreamOutput;
-use Symfony\Component\Yaml\Yaml;
 
 abstract class PlatformCommand extends Command
 {
