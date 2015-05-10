5d3d7436e389528e110e01a832545c1b004f9619
Patrick Dawkins <pjcdawkins@gmail.com>
2015-05-10 13:23:42 +0100

    Release v2.0.2
    Fixes autocompletion bug

--- src/Application.php
+++ src/Application.php
@@ -25,7 +25,7 @@ class Application extends ParentApplication
      */
     public function __construct()
     {
-        parent::__construct('Platform.sh CLI', '2.0.1');
+        parent::__construct('Platform.sh CLI', '2.0.2');
 
         $this->setDefaultTimezone();
 
