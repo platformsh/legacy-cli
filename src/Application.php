20b92b400324adbb6ce49e7046162fd1674c21ec
Patrick Dawkins <pjcdawkins@gmail.com>
2015-05-20 18:04:44 +0100

    Release 2.0.4

--- src/Application.php
+++ src/Application.php
@@ -25,7 +25,7 @@ class Application extends ParentApplication
      */
     public function __construct()
     {
-        parent::__construct('Platform.sh CLI', '2.0.3');
+        parent::__construct('Platform.sh CLI', '2.0.4');
 
         $this->setDefaultTimezone();
 
