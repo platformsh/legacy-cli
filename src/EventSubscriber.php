13b353604d9f184d4f96b83c5958ce96f3943934
Patrick Dawkins <pjcdawkins@gmail.com>
2015-07-24 17:48:07 -0400

    Wrap OAuth2 400/401 errors

--- src/EventSubscriber.php
+++ src/EventSubscriber.php
@@ -1,8 +1,11 @@
 <?php
 namespace Platformsh\Cli;
 
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ParseException;
 use Platformsh\Cli\Exception\ConnectionFailedException;
+use Platformsh\Cli\Exception\LoginRequiredException;
 use Symfony\Component\Console\Event\ConsoleExceptionEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -36,6 +39,32 @@ class EventSubscriber implements EventSubscriberInterface
               . " \nPlease check your Internet connection"
             ));
             $event->stopPropagation();
+        }
+
+        // Handle Guzzle client exceptions, i.e. HTTP 4xx errors.
+        if ($exception instanceof ClientException && ($response = $exception->getResponse())) {
+            try {
+                $response->getBody()->seek(0);
+                $json = $response->json();
+            }
+            catch (ParseException $e) {
+                $json = [];
+            }
+
+            // Create a friendlier message for the OAuth2 "Invalid refresh token"
+            // error.
+            if ($response->getStatusCode() === 400 && isset($json['error_description']) && $json['error_description'] === 'Invalid refresh token') {
+                $event->setException(new LoginRequiredException(
+                    "Invalid refresh token: please log in again."
+                ));
+                $event->stopPropagation();
+            }
+            elseif ($response->getStatusCode() === 401) {
+                $event->setException(new LoginRequiredException(
+                    "Unauthorized: please log in again."
+                ));
+                $event->stopPropagation();
+            }
         }
     }
 }
