154ff8c08fdedf64cc6696ebcd9fee072f5fc959
Patrick Dawkins <pjcdawkins@gmail.com>
2015-11-22 11:12:23 +0000

    Add bot animation

---
+++ src/Command/BotCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Platformsh\Cli\Command;
+
+use Platformsh\Cli\Util\Bot;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BotCommand extends PlatformCommand
+{
+
+    protected function configure()
+    {
+        $this->setName('bot')->setDescription('The Platform.sh Bot');
+        $this->setHiddenInList();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $bot = new Bot($this->stdErr);
+        while (true) {
+            $bot->displayNext();
+        }
+    }
+}
---
+++ src/Util/Bot.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Platformsh\Cli\Util;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Bot extends ConsoleAnimation
+{
+    /**
+     * @param OutputInterface $output
+     */
+    public function __construct(OutputInterface $output)
+    {
+        $interval = 500000;
+        $filenames = [
+          CLI_ROOT . '/resources/bot/bot1',
+          CLI_ROOT . '/resources/bot/bot2',
+          CLI_ROOT . '/resources/bot/bot3',
+          CLI_ROOT . '/resources/bot/bot4',
+        ];
+
+        // The frames are the contents of each file, with each line indented.
+        $frames = array_map(function ($filename) {
+            return preg_replace('/^/m', '    ', file_get_contents($filename));
+        }, $filenames);
+
+        parent::__construct($output, $interval, $frames);
+    }
+}
