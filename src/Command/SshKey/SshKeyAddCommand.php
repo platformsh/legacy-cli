1976b462045de89d175a47db2c2be11c9c29d105
Patrick Dawkins <pjcdawkins@gmail.com>
2015-11-27 10:14:04 +0000

    Minor bugfix in ssh-key:add
    And a variable name and whitespace change.

--- src/Command/SshKey/SshKeyAddCommand.php
+++ src/Command/SshKey/SshKeyAddCommand.php
@@ -30,55 +30,49 @@ class SshKeyAddCommand extends PlatformCommand
         /** @var \Platformsh\Cli\Helper\PlatformQuestionHelper $questionHelper */
         $questionHelper = $this->getHelper('question');
 
-        $path = $input->getArgument('path');
-        if (empty($path)) {
+        $publicKeyPath = $input->getArgument('path');
+        if (empty($publicKeyPath)) {
             $shellHelper = $this->getHelper('shell');
             $default = $this->getDefaultKeyFilename();
             $defaultPath = "$default.pub";
 
             // Look for an existing local key.
-            if (file_exists($defaultPath) && $questionHelper->confirm(
-                "Use existing local key <info>" . basename($defaultPath) . "</info>?",
-                $input,
-                $output
-              )
-            ) {
-                $path = $defaultPath;
-            } // Offer to generate a key.
-            elseif ($shellHelper->commandExists('ssh-keygen') && $shellHelper->commandExists(
-                'ssh-add'
-              ) && $questionHelper->confirm("Generate a new key?", $input, $this->stdErr)
-            ) {
+            if (file_exists($defaultPath)
+              && $questionHelper->confirm("Use existing local key <info>" . basename($defaultPath) . "</info>?", $input, $output)) {
+                $publicKeyPath = $defaultPath;
+            }
+            // Offer to generate a key.
+            elseif ($shellHelper->commandExists('ssh-keygen')
+              && $shellHelper->commandExists('ssh-add')
+              && $questionHelper->confirm("Generate a new key?", $input, $this->stdErr)) {
                 $newKey = $this->getNewKeyFilename($default);
                 $args = array('ssh-keygen', '-t', 'rsa', '-f', $newKey, '-N', '');
                 $shellHelper->execute($args, null, true);
-                $this->stdErr->writeln("Generated a new key: $newKey.pub");
+                $publicKeyPath = $newKey . '.pub';
+                $this->stdErr->writeln("Generated a new key: $publicKeyPath");
                 $this->stdErr->writeln('Add this key to an SSH agent with:');
                 $this->stdErr->writeln('    eval $(ssh-agent)');
                 $this->stdErr->writeln('    ssh-add ' . escapeshellarg($newKey));
-            } else {
+            }
+            else {
                 $this->stdErr->writeln("<error>You must specify the path to a public SSH key</error>");
-
                 return 1;
             }
-
         }
 
-        if (!file_exists($path)) {
-            $this->stdErr->writeln("File not found: <error>$path<error>");
-
+        if (!file_exists($publicKeyPath)) {
+            $this->stdErr->writeln("File not found: <error>$publicKeyPath<error>");
             return 1;
         }
 
-        $process = new Process('ssh-keygen -l -f ' . escapeshellarg($path));
+        $process = new Process('ssh-keygen -l -f ' . escapeshellarg($publicKeyPath));
         $process->run();
         if ($process->getExitCode() == 1) {
-            $this->stdErr->writeln("The file does not contain a valid public key: <error>$path</error>");
-
+            $this->stdErr->writeln("The file does not contain a valid public key: <error>$publicKeyPath</error>");
             return 1;
         }
 
-        $key = file_get_contents($path);
+        $key = file_get_contents($publicKeyPath);
 
         $name = $input->getOption('name');
         if (!$name) {
@@ -90,7 +84,7 @@ class SshKeyAddCommand extends PlatformCommand
              ->addSshKey($key, $name);
 
         $this->stdErr->writeln(
-          'The SSH key <info>' . basename($path) . '</info> has been successfully added to your Platform.sh account'
+          'The SSH key <info>' . basename($publicKeyPath) . '</info> has been successfully added to your Platform.sh account'
         );
 
         return 0;
