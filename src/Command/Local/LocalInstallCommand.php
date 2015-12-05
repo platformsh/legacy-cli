27b932a97afd4c7fbc23a98b6af9a63cb31d3cd2
Patrick Dawkins <pjcdawkins@gmail.com>
2015-12-05 22:06:43 +0000

    Improve local:install UX (inspired by 'drush init')

--- src/Command/Local/LocalInstallCommand.php
+++ src/Command/Local/LocalInstallCommand.php
@@ -13,7 +13,12 @@ class LocalInstallCommand extends CommandBase
     protected function configure()
     {
         $this->setName('local:install')
-             ->setDescription('Install CLI configuration files');
+             ->setDescription('Install or update CLI configuration files');
+        $this->setHelp(<<<EOT
+This command automatically installs shell configuration for the Platform.sh CLI,
+adding autocompletion support and handy aliases. Bash and ZSH are supported.
+EOT
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -33,22 +38,45 @@ class LocalInstallCommand extends CommandBase
             return 1;
         }
 
+        $this->stdErr->writeln(sprintf('Successfully copied CLI configuration to: %s', $platformRcDestination));
+
         if (!$shellConfigFile = $this->findShellConfigFile($homeDir)) {
             $this->stdErr->writeln('Failed to find a shell configuration file.');
             return 1;
         }
 
-        $shellConfig = file_get_contents($shellConfigFile);
-        if (strpos($shellConfig, $configDir . "/bin") !== false) {
-            $this->stdErr->writeln(sprintf('Already configured: %s', $shellConfigFile));
+        $this->stdErr->writeln(sprintf('Reading shell configuration file: %s', $shellConfigFile));
+
+        $currentShellConfig = file_get_contents($shellConfigFile);
+        if (strpos($currentShellConfig, $configDir . "/bin") !== false) {
+            $this->stdErr->writeln(sprintf('Already configured: <info>%s</info>', $shellConfigFile));
             return 0;
         }
 
-        $shellConfig .= PHP_EOL . PHP_EOL
-            . "# Automatically added by Platform.sh CLI installer" . PHP_EOL
-            . "export PATH=\"$configDir/bin:\$PATH\"" . PHP_EOL
-            . '. ' . escapeshellarg($platformRcDestination) . " 2>/dev/null" . PHP_EOL;
-        if (!file_put_contents($shellConfigFile, $shellConfig)) {
+        $suggestedShellConfig = "export PATH=\"$configDir/bin:\$PATH\"" . PHP_EOL
+            . '. ' . escapeshellarg($platformRcDestination) . " 2>/dev/null";
+
+        /** @var \Platformsh\Cli\Helper\PlatformQuestionHelper $questionHelper */
+        $questionHelper = $this->getHelper('question');
+        if (!$questionHelper->confirm('Do you want to update the file automatically?', $input, $this->stdErr)) {
+            $suggestedShellConfig = PHP_EOL
+                . '# Platform.sh CLI configuration'
+                . PHP_EOL
+                . $suggestedShellConfig;
+
+            $this->stdErr->writeln(sprintf('To set up the CLI, add the following lines to: <comment>%s</comment>', $shellConfigFile));
+            $this->stdErr->writeln(preg_replace('/^/m', '  ', $suggestedShellConfig));
+            return 1;
+        }
+
+        $newShellConfig = rtrim($currentShellConfig, PHP_EOL)
+            . PHP_EOL . PHP_EOL
+            . '# Automatically added by the Platform.sh CLI'
+            . PHP_EOL . $suggestedShellConfig;
+
+        copy($shellConfigFile, $shellConfigFile . '.cli.bak');
+
+        if (!file_put_contents($shellConfigFile, $newShellConfig)) {
             $this->stdErr->writeln(sprintf('Failed to modify configuration file: %s', $shellConfigFile));
             return 1;
         }
@@ -57,10 +85,13 @@ class LocalInstallCommand extends CommandBase
         if (getcwd() === dirname($shellConfigFile)) {
             $shortPath = basename($shellConfigFile);
         }
+        if (strpos($shortPath, ' ')) {
+            $shortPath = escapeshellarg($shortPath);
+        }
 
-        $this->stdErr->writeln('Start a new terminal to use the new configuration.');
+        $this->stdErr->writeln("Updated successfully. Start a new terminal to use the new configuration.");
         $this->stdErr->writeln('Or to use it now, type:');
-        $this->stdErr->writeln('  <info>source ' . escapeshellarg($shortPath) . '</info>');
+        $this->stdErr->writeln('  <info>source ' . $shortPath . '</info>');
 
         return 0;
     }
