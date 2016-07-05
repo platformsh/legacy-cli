platformsh/legacy-cli

commit 3b2d61238347b2cc1c614af9ca10cec902908d0d

Account for orphaned environments in environment:list

src/Command/Environment/EnvironmentListCommand.php
@@ -149,6 +149,10 @@ class EnvironmentListCommand extends CommandBase
             $this->children['master'] = [];
         }
 
+        // Add remaining environments that were left out of the tree (e.g.
+        // environments whose parent does not exist).
+        $tree += $environments;
+
         $headers = ['ID', 'Name', 'Status'];
 
         $table = new Table($input, $output);

commit 26e03549387517e204b1651d3f395ef8bbd1d49c

Fix environments duplicated in list (caused by 3b2d61238347b2cc1c614af9ca10cec902908d0d)

src/Command/Environment/EnvironmentListCommand.php
@@ -149,9 +149,13 @@ class EnvironmentListCommand extends CommandBase
             $this->children['master'] = [];
         }
 
-        // Add remaining environments that were left out of the tree (e.g.
-        // environments whose parent does not exist).
-        $tree += $environments;
+        // Add orphaned environments (those whose parents do not exist) to the
+        // tree.
+        foreach ($environments as $id => $environment) {
+            if (!empty($environment->parent) && !isset($environments[$environment->parent])) {
+                $tree += [$id => $environment];
+            }
+        }
 
         $headers = ['ID', 'Name', 'Status'];
 