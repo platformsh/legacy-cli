ba7baa0d48cc4321a12658ac2ecbb39b8ed23512
Ariel Barreiro <abarrei@gmail.com>
2016-07-21 13:43:55 -0300

    Rework ssh url to use already made function, and add relationships for tunnel info

--- src/Command/Project/ProjectSshConfigCommand.php
+++ src/Command/Project/ProjectSshConfigCommand.php
@@ -3,6 +3,8 @@ namespace Platformsh\Cli\Command\Project;
 
 use Platformsh\Cli\Command\CommandBase;
 use Platformsh\Cli\Console\AdaptiveTableCell;
+use Platformsh\Cli\Util\PortUtil;
+use Platformsh\Cli\Util\RelationshipsUtil;
 use Platformsh\Cli\Util\Table;
 use Platformsh\Client\Model\Project;
 use Symfony\Component\Console\Input\InputInterface;
@@ -20,6 +22,25 @@ class ProjectSshConfigCommand extends CommandBase
             ->setAliases(['ssh-config'])
             ->setDescription('outputs OpenSSH valid configuration to connect all of the project environments');
         $this->addProjectOption();
+        $this->addAppOption();
+    }
+
+    /**
+     * Automatically determine the best port for a new tunnel.
+     *
+     * @param int $default
+     *
+     * @return int
+     */
+    protected function getPort($default = 30000)
+    {
+        static $ports = [];
+        $port = PortUtil::getPort($ports ? max($ports) + 1 : $default);
+        if (!in_array($port, $ports)) {
+            $ports[] = $port;
+        }
+
+        return $port;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -27,7 +48,8 @@ class ProjectSshConfigCommand extends CommandBase
         $this->validateInput($input);
         $project = $this->getSelectedProject();
 
-        $environments = $this->api()->getEnvironments($this->getSelectedProject(), $refresh ? true : null);
+        // Always refresh to make sure we have the most up to date data
+        $environments = $this->api()->getEnvironments($project, true);
 
         // Filter out inactive environments
         $environments = array_filter($environments, function ($environment) {
@@ -42,15 +64,30 @@ class ProjectSshConfigCommand extends CommandBase
             $alias = isset($projectConfig['alias-group']) ? $projectConfig['alias-group'] : $project->getProperty('id');;
         }
 
+        $appName = $this->selectApp($input);
+
         foreach ($environments as $environment) {
             if ($environment->hasLink('ssh')) {
-                $getLink = $environment->getLink('ssh');
-                $sshUrl = parse_url($environment->getLink('ssh'));
+                $sshUrl = $environment->getSshUrl($appName);
+                $sshUrlParts = split("@", $sshUrl);
                 $indent = str_repeat(' ', 2);
 
                 $output->writeln("Host $alias.{$environment->id}");
-                $output->writeln($indent . "Hostname {$sshUrl['host']}");
-                $output->writeln($indent . "User {$sshUrl['user']}");
+                $output->writeln($indent . "Hostname {$sshUrlParts[0]}");
+                $output->writeln($indent . "User {$sshUrlParts[1]}");
+
+                $util = new RelationshipsUtil($this->stdErr);
+                $relationships = $util->getRelationships($sshUrl);
+
+                if ($relationships) {
+                    foreach ($relationships as $relationship => $services) {
+                        foreach ($services as $serviceKey => $service) {
+                            $localPort = $this->getPort();
+                            $output->writeln($indent . "LocalForward $localPort {$service['host']}:{$service['port']}");
+                        }
+                    }
+                }
+
                 $output->writeln('');
             }
         }
