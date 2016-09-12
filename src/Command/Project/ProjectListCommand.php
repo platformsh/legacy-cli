3d95c9fcf66779a35fa736cd4aeba4367009d541
Vincenzo Russo <v@artetecha.com>
2016-09-12 14:38:11 +0100

    VALUE_REQUIRED for host and title filter options.

--- src/Command/Project/ProjectListCommand.php
+++ src/Command/Project/ProjectListCommand.php
@@ -19,8 +19,8 @@ class ProjectListCommand extends CommandBase
             ->setAliases(['projects'])
             ->setDescription('Get a list of all active projects')
             ->addOption('pipe', null, InputOption::VALUE_NONE, 'Output a simple list of project IDs')
-            ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'Filter by region hostname')
-            ->addOption('title', null, InputOption::VALUE_OPTIONAL, 'Filter by title')
+            ->addOption('host', null, InputOption::VALUE_REQUIRED, 'Filter by region hostname')
+            ->addOption('title', null, InputOption::VALUE_REQUIRED, 'Filter by title')
             ->addOption('refresh', null, InputOption::VALUE_REQUIRED, 'Whether to refresh the list', 1)
             ->addOption('sort', null, InputOption::VALUE_REQUIRED, 'A property to sort by', 'title')
             ->addOption('reverse', null, InputOption::VALUE_NONE, 'Sort in reverse (descending) order');
