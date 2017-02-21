ksjogo/platformsh-cli

Update for new DIC

src/Command/Server/ServerCommandBase.php
@@ -15,8 +15,8 @@ abstract class ServerCommandBase extends CommandBase
 
     public function isEnabled()
     {
-        return self::$config->has('experimental.enable_local_server')
-            && self::$config->get('experimental.enable_local_server')
+        return $this->config()->has('experimental.enable_local_server')
+            && $this->config()->get('experimental.enable_local_server')
             && parent::isEnabled();
     }
 
@@ -56,15 +56,13 @@ abstract class ServerCommandBase extends CommandBase
         $serverInfo = $this->getServerInfo();
         if (file_exists($pidFile)) {
             $pid = file_get_contents($pidFile);
-        }
-        elseif (isset($serverInfo[$address])) {
+        } elseif (isset($serverInfo[$address])) {
             $pid = $serverInfo[$address]['pid'];
         }
 
         if (!empty($pid) && (!function_exists('posix_kill') || posix_kill($pid, 0))) {
             return $pid;
-        }
-        elseif (!empty($pid)) {
+        } elseif (!empty($pid)) {
             // The PID is no longer valid. Delete the lock file and
             // continue.
             $this->stopServer($address);
@@ -89,9 +87,9 @@ abstract class ServerCommandBase extends CommandBase
     {
         if (!isset($this->serverInfo)) {
             $this->serverInfo = [];
-            $filename = self::$config->getUserConfigDir() . '/local-servers.json';
+            $filename = $this->config()->getUserConfigDir() . '/local-servers.json';
             if (file_exists($filename)) {
-                $this->serverInfo = (array) json_decode(file_get_contents($filename), TRUE);
+                $this->serverInfo = (array) json_decode(file_get_contents($filename), true);
             }
         }
 
@@ -111,13 +109,12 @@ abstract class ServerCommandBase extends CommandBase
 
     protected function saveServerInfo()
     {
-        $filename = self::$config->getUserConfigDir() . '/local-servers.json';
+        $filename = $this->config()->getUserConfigDir() . '/local-servers.json';
         if (!empty($this->serverInfo)) {
             if (!file_put_contents($filename, json_encode($this->serverInfo))) {
                 throw new \RuntimeException('Failed to write server info to: ' . $filename);
             }
-        }
-        else {
+        } else {
             unlink($filename);
         }
     }
@@ -194,7 +191,7 @@ abstract class ServerCommandBase extends CommandBase
      */
     protected function getPidFile($address)
     {
-        return self::$config->getUserConfigDir() . '/server-' . preg_replace('/\W+/', '-', $address) . '.pid';
+        return $this->config()->getUserConfigDir() . '/server-' . preg_replace('/\W+/', '-', $address) . '.pid';
     }
 
     /**
@@ -221,9 +218,11 @@ abstract class ServerCommandBase extends CommandBase
                 $stack = 'php';
             }
             if ($stack === 'php' && $version && version_compare(PHP_VERSION, $version, '<')) {
-                $this->stdErr->writeln(
-                  sprintf("<comment>Warning:</comment> your local PHP version is %s, but the app expects %s", PHP_VERSION, $version)
-                );
+                $this->stdErr->writeln(sprintf(
+                    '<comment>Warning:</comment> your local PHP version is %s, but the app expects %s',
+                    PHP_VERSION,
+                    $version
+                ));
             }
         }
 
@@ -234,7 +233,7 @@ abstract class ServerCommandBase extends CommandBase
 
             $this->showSecurityWarning();
 
-            $arguments[] = $this->getHelper('shell')->resolveCommand('php');
+            $arguments[] = $this->getService('shell')->resolveCommand('php');
 
             foreach ($this->getServerPhpConfig() as $item => $value) {
                 $arguments[] = sprintf('-d %s="%s"', $item, $value);
@@ -255,12 +254,12 @@ abstract class ServerCommandBase extends CommandBase
 
             $builder = new ProcessBuilder($arguments);
             $process = $builder->getProcess();
-        }
-        else {
+        } else {
             // Bail out. We can't support non-PHP apps for now.
-            throw new \Exception(
-              sprintf("Not supported: the CLI doesn't yet support starting a server for the application type '%s'", $appConfig['type'])
-            );
+            throw new \Exception(sprintf(
+                "Not supported: the CLI doesn't yet support starting a server for the application type '%s'",
+                $appConfig['type']
+            ));
 
             // The following code is a potential strategy for non-PHP apps, but
             // it won't really work without starting more than one process,
@@ -278,7 +277,7 @@ abstract class ServerCommandBase extends CommandBase
         $process->setTimeout(null);
         $env += $this->createEnv($projectRoot, $docRoot, $address, $appConfig);
         $process->setEnv($env);
-        $envPrefix = self::$config->get('application.env_prefix');
+        $envPrefix = $this->config()->get('application.env_prefix');
         if (isset($env[$envPrefix . '_APP_DIR'])) {
             $process->setWorkingDirectory($env[$envPrefix . '_APP_DIR']);
         }
@@ -323,7 +322,7 @@ abstract class ServerCommandBase extends CommandBase
             return false;
         }
 
-        $router = $projectRoot . '/' . self::$config->get('local.local_dir') . '/' . basename($router_src);
+        $router = $projectRoot . '/' . $this->config()->get('local.local_dir') . '/' . basename($router_src);
         if (!isset($created[$router])) {
             if (!file_put_contents($router, file_get_contents($router_src))) {
                 $this->stdErr->writeln(sprintf('Could not create router file: <error>%s</error>', $router));
@@ -363,7 +362,7 @@ abstract class ServerCommandBase extends CommandBase
     protected function createEnv($projectRoot, $docRoot, $address, array $appConfig)
     {
         $realDocRoot = realpath($docRoot);
-        $envPrefix = self::$config->get('application.env_prefix');
+        $envPrefix = $this->config()->get('application.env_prefix');
         $env = [
             '_PLATFORM_VARIABLES_PREFIX' => $envPrefix,
             $envPrefix . 'ENVIRONMENT' => '_local',
@@ -374,7 +373,7 @@ abstract class ServerCommandBase extends CommandBase
 
         list($env['IP'], $env['PORT']) = explode(':', $address);
 
-        if (dirname(dirname($realDocRoot)) === $projectRoot . '/' . self::$config->get('local.local_dir')) {
+        if (dirname(dirname($realDocRoot)) === $projectRoot . '/' . $this->config()->get('local.local_dir')) {
             $env[$envPrefix . 'APP_DIR'] = dirname($realDocRoot);
         }
 
@@ -384,8 +383,7 @@ abstract class ServerCommandBase extends CommandBase
                 if ($project) {
                     $env[$envPrefix . 'PROJECT'] = $project->id;
                 }
-            }
-            catch (\Exception $e) {
+            } catch (\Exception $e) {
                 // Ignore errors
             }
         }
@@ -399,7 +397,9 @@ abstract class ServerCommandBase extends CommandBase
         if ($shown) {
             return;
         }
-        $this->stdErr->writeln('<comment>Warning:</comment> this uses the PHP built-in web server, which is neither secure nor reliable for production use');
+        $this->stdErr->writeln(
+            '<comment>Warning:</comment> this uses the PHP built-in web server, which is neither secure nor reliable for production use'
+        );
         $shown = true;
     }
 }

src/Command/Server/ServerStartCommand.php
@@ -21,8 +21,8 @@ class ServerStartCommand extends ServerCommandBase
           ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force starting servers')
           ->addOption('ip', null, InputOption::VALUE_REQUIRED, 'The IP address', '127.0.0.1')
           ->addOption('port', null, InputOption::VALUE_REQUIRED, 'The port of the first server')
-          ->addOption('log', null, InputOption::VALUE_REQUIRED, 'The name of a log file. Defaults to ' . self::$config->get('local.local_dir') . '/server.log')
-          ->addOption('tunnel', null, InputOption::VALUE_NONE, 'Incorporate SSH tunnels to remote ' . self::$config->get('service.name') . ' environments as relationships');
+          ->addOption('log', null, InputOption::VALUE_REQUIRED, 'The name of a log file. Defaults to ' . $this->config()->get('local.local_dir') . '/server.log')
+          ->addOption('tunnel', null, InputOption::VALUE_NONE, 'Incorporate SSH tunnels to remote ' . $this->config()->get('service.name') . ' environments as relationships');
     }
 
     public function isEnabled()
@@ -56,7 +56,7 @@ class ServerStartCommand extends ServerCommandBase
         }
 
         $multiApp = count($apps) > 1;
-        $webRoot = $projectRoot . '/' . self::$config->get('local.web_root');
+        $webRoot = $projectRoot . '/' . $this->config()->get('local.web_root');
         $items = [];
         foreach ($apps as $app) {
             $appId = $app->getId();
@@ -86,12 +86,12 @@ class ServerStartCommand extends ServerCommandBase
                 );
                 if ($result != 0) {
                     $this->stdErr->writeln(sprintf('Failed to get SSH tunnel information for the app <error>%s</error>', $appId));
-                    $this->stdErr->writeln('Run <comment>' . self::$config->get('application.executable') . ' tunnel:open</comment> to create tunnels.');
+                    $this->stdErr->writeln('Run <comment>' . $this->config()->get('application.executable') . ' tunnel:open</comment> to create tunnels.');
                     unset($items[$appId]);
                     continue;
                 }
                 $relationships = $bufferedOutput->fetch();
-                $items[$appId]['env'][self::$config->get('application.env_prefix') . 'RELATIONSHIPS'] = $relationships;
+                $items[$appId]['env'][$this->config()->get('application.env_prefix') . 'RELATIONSHIPS'] = $relationships;
             }
         }
 
@@ -99,7 +99,7 @@ class ServerStartCommand extends ServerCommandBase
             return 1;
         }
 
-        $logFile = $input->getOption('log') ?: $projectRoot . '/' . self::$config->get('local.local_dir') . '/server.log';
+        $logFile = $input->getOption('log') ?: $projectRoot . '/' . $this->config()->get('local.local_dir') . '/server.log';
         $log = $this->openLog($logFile);
         if (!$log) {
             $this->stdErr->writeln(sprintf('Failed to open log file for writing: <error>%s</error>', $logFile));
@@ -123,10 +123,10 @@ class ServerStartCommand extends ServerCommandBase
             if ($otherServer = $this->isServerRunningForApp($appId, $projectRoot)) {
                 if (!$force) {
                     $this->stdErr->writeln(sprintf(
-                      'A server is already running for the app <info>%s</info> at http://%s, PID %s',
-                      $appId,
-                      $otherServer['address'],
-                      $otherServer['pid']
+                        'A server is already running for the app <info>%s</info> at http://%s, PID %s',
+                        $appId,
+                        $otherServer['address'],
+                        $otherServer['pid']
                     ));
                     continue;
                 }
@@ -134,9 +134,10 @@ class ServerStartCommand extends ServerCommandBase
                 // If the port was not manually specified, kill the old server and
                 // take its address.
                 $this->stdErr->writeln(sprintf(
-                  'Stopping server for the app <info>%s</info> at http://%s',
-                  $appId,
-                  $otherServer['address']));
+                    'Stopping server for the app <info>%s</info> at http://%s',
+                    $appId,
+                    $otherServer['address']
+                ));
                 $this->stopServer($address, $otherServer['pid']);
                 sleep(1);
 
@@ -145,13 +146,12 @@ class ServerStartCommand extends ServerCommandBase
                 if (!$input->getOption('port') && $input->getOption('ip') === '127.0.0.1') {
                     $address = $otherServer['address'];
                 }
-            }
-            elseif ($otherPid = $this->isServerRunningForAddress($address)) {
+            } elseif ($otherPid = $this->isServerRunningForAddress($address)) {
                 if (!$force || $otherPid === true) {
                     $this->stdErr->writeln(sprintf(
-                      'A server is already running at address: http://%s, PID %s',
-                      $address,
-                      $otherPid === true ? 'unknown' : $otherPid
+                        'A server is already running at address: http://%s, PID %s',
+                        $address,
+                        $otherPid === true ? 'unknown' : $otherPid
                     ));
                     $error = true;
                     continue;
@@ -167,8 +167,7 @@ class ServerStartCommand extends ServerCommandBase
 
             try {
                 $processManager->startProcess($process, $pidFile, $log);
-            }
-            catch (\Exception $e) {
+            } catch (\Exception $e) {
                 $this->stdErr->writeln(sprintf('Failed to start server: %s', $e->getMessage()));
                 unset($processes[$address]);
                 $error = true;
@@ -199,7 +198,7 @@ class ServerStartCommand extends ServerCommandBase
 
         if (count($processes)) {
             $this->stdErr->writeln(sprintf('Logs are written to: %s', $logFile));
-            $this->stdErr->writeln('Use <comment>' . self::$config->get('application.executable') . ' server:stop</comment> to stop server(s)');
+            $this->stdErr->writeln('Use <comment>' . $this->config()->get('application.executable') . ' server:stop</comment> to stop server(s)');
         }
 
         // The terminal has received all necessary output, so we can stop the

platformsh/legacy-cli

src/Command/Server/ServerCommandBase.php
@@ -15,8 +15,8 @@ abstract class ServerCommandBase extends CommandBase
 
     public function isEnabled()
     {
-        return self::$config->has('experimental.enable_local_server')
-            && self::$config->get('experimental.enable_local_server')
+        return $this->config()->has('experimental.enable_local_server')
+            && $this->config()->get('experimental.enable_local_server')
             && parent::isEnabled();
     }
 
@@ -56,15 +56,13 @@ abstract class ServerCommandBase extends CommandBase
         $serverInfo = $this->getServerInfo();
         if (file_exists($pidFile)) {
             $pid = file_get_contents($pidFile);
-        }
-        elseif (isset($serverInfo[$address])) {
+        } elseif (isset($serverInfo[$address])) {
             $pid = $serverInfo[$address]['pid'];
         }
 
         if (!empty($pid) && (!function_exists('posix_kill') || posix_kill($pid, 0))) {
             return $pid;
-        }
-        elseif (!empty($pid)) {
+        } elseif (!empty($pid)) {
             // The PID is no longer valid. Delete the lock file and
             // continue.
             $this->stopServer($address);
@@ -89,9 +87,9 @@ abstract class ServerCommandBase extends CommandBase
     {
         if (!isset($this->serverInfo)) {
             $this->serverInfo = [];
-            $filename = self::$config->getUserConfigDir() . '/local-servers.json';
+            $filename = $this->config()->getUserConfigDir() . '/local-servers.json';
             if (file_exists($filename)) {
-                $this->serverInfo = (array) json_decode(file_get_contents($filename), TRUE);
+                $this->serverInfo = (array) json_decode(file_get_contents($filename), true);
             }
         }
 
@@ -111,13 +109,12 @@ abstract class ServerCommandBase extends CommandBase
 
     protected function saveServerInfo()
     {
-        $filename = self::$config->getUserConfigDir() . '/local-servers.json';
+        $filename = $this->config()->getUserConfigDir() . '/local-servers.json';
         if (!empty($this->serverInfo)) {
             if (!file_put_contents($filename, json_encode($this->serverInfo))) {
                 throw new \RuntimeException('Failed to write server info to: ' . $filename);
             }
-        }
-        else {
+        } else {
             unlink($filename);
         }
     }
@@ -194,7 +191,7 @@ abstract class ServerCommandBase extends CommandBase
      */
     protected function getPidFile($address)
     {
-        return self::$config->getUserConfigDir() . '/server-' . preg_replace('/\W+/', '-', $address) . '.pid';
+        return $this->config()->getUserConfigDir() . '/server-' . preg_replace('/\W+/', '-', $address) . '.pid';
     }
 
     /**
@@ -221,9 +218,11 @@ abstract class ServerCommandBase extends CommandBase
                 $stack = 'php';
             }
             if ($stack === 'php' && $version && version_compare(PHP_VERSION, $version, '<')) {
-                $this->stdErr->writeln(
-                  sprintf("<comment>Warning:</comment> your local PHP version is %s, but the app expects %s", PHP_VERSION, $version)
-                );
+                $this->stdErr->writeln(sprintf(
+                    '<comment>Warning:</comment> your local PHP version is %s, but the app expects %s',
+                    PHP_VERSION,
+                    $version
+                ));
             }
         }
 
@@ -234,7 +233,7 @@ abstract class ServerCommandBase extends CommandBase
 
             $this->showSecurityWarning();
 
-            $arguments[] = $this->getHelper('shell')->resolveCommand('php');
+            $arguments[] = $this->getService('shell')->resolveCommand('php');
 
             foreach ($this->getServerPhpConfig() as $item => $value) {
                 $arguments[] = sprintf('-d %s="%s"', $item, $value);
@@ -255,12 +254,12 @@ abstract class ServerCommandBase extends CommandBase
 
             $builder = new ProcessBuilder($arguments);
             $process = $builder->getProcess();
-        }
-        else {
+        } else {
             // Bail out. We can't support non-PHP apps for now.
-            throw new \Exception(
-              sprintf("Not supported: the CLI doesn't yet support starting a server for the application type '%s'", $appConfig['type'])
-            );
+            throw new \Exception(sprintf(
+                "Not supported: the CLI doesn't yet support starting a server for the application type '%s'",
+                $appConfig['type']
+            ));
 
             // The following code is a potential strategy for non-PHP apps, but
             // it won't really work without starting more than one process,
@@ -278,7 +277,7 @@ abstract class ServerCommandBase extends CommandBase
         $process->setTimeout(null);
         $env += $this->createEnv($projectRoot, $docRoot, $address, $appConfig);
         $process->setEnv($env);
-        $envPrefix = self::$config->get('application.env_prefix');
+        $envPrefix = $this->config()->get('service.env_prefix');
         if (isset($env[$envPrefix . '_APP_DIR'])) {
             $process->setWorkingDirectory($env[$envPrefix . '_APP_DIR']);
         }
@@ -323,7 +322,7 @@ abstract class ServerCommandBase extends CommandBase
             return false;
         }
 
-        $router = $projectRoot . '/' . self::$config->get('local.local_dir') . '/' . basename($router_src);
+        $router = $projectRoot . '/' . $this->config()->get('local.local_dir') . '/' . basename($router_src);
         if (!isset($created[$router])) {
             if (!file_put_contents($router, file_get_contents($router_src))) {
                 $this->stdErr->writeln(sprintf('Could not create router file: <error>%s</error>', $router));
@@ -363,7 +362,7 @@ abstract class ServerCommandBase extends CommandBase
     protected function createEnv($projectRoot, $docRoot, $address, array $appConfig)
     {
         $realDocRoot = realpath($docRoot);
-        $envPrefix = self::$config->get('application.env_prefix');
+        $envPrefix = $this->config()->get('service.env_prefix');
         $env = [
             '_PLATFORM_VARIABLES_PREFIX' => $envPrefix,
             $envPrefix . 'ENVIRONMENT' => '_local',
@@ -374,7 +373,7 @@ abstract class ServerCommandBase extends CommandBase
 
         list($env['IP'], $env['PORT']) = explode(':', $address);
 
-        if (dirname(dirname($realDocRoot)) === $projectRoot . '/' . self::$config->get('local.local_dir')) {
+        if (dirname(dirname($realDocRoot)) === $projectRoot . '/' . $this->config()->get('local.local_dir')) {
             $env[$envPrefix . 'APP_DIR'] = dirname($realDocRoot);
         }
 
@@ -384,8 +383,7 @@ abstract class ServerCommandBase extends CommandBase
                 if ($project) {
                     $env[$envPrefix . 'PROJECT'] = $project->id;
                 }
-            }
-            catch (\Exception $e) {
+            } catch (\Exception $e) {
                 // Ignore errors
             }
         }
@@ -399,7 +397,9 @@ abstract class ServerCommandBase extends CommandBase
         if ($shown) {
             return;
         }
-        $this->stdErr->writeln('<comment>Warning:</comment> this uses the PHP built-in web server, which is neither secure nor reliable for production use');
+        $this->stdErr->writeln(
+            '<comment>Warning:</comment> this uses the PHP built-in web server, which is neither secure nor reliable for production use'
+        );
         $shown = true;
     }
 }

src/Command/Server/ServerStartCommand.php
@@ -5,7 +5,7 @@ use Platformsh\Cli\Exception\RootNotFoundException;
 use Platformsh\Cli\Local\LocalApplication;
 use Platformsh\Cli\Local\Toolstack\Drupal;
 use Platformsh\Cli\Util\PortUtil;
-use Platformsh\Cli\Util\ProcessManager;
+use Platformsh\Cli\Console\ProcessManager;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\BufferedOutput;
@@ -21,8 +21,8 @@ class ServerStartCommand extends ServerCommandBase
           ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force starting servers')
           ->addOption('ip', null, InputOption::VALUE_REQUIRED, 'The IP address', '127.0.0.1')
           ->addOption('port', null, InputOption::VALUE_REQUIRED, 'The port of the first server')
-          ->addOption('log', null, InputOption::VALUE_REQUIRED, 'The name of a log file. Defaults to ' . self::$config->get('local.local_dir') . '/server.log')
-          ->addOption('tunnel', null, InputOption::VALUE_NONE, 'Incorporate SSH tunnels to remote ' . self::$config->get('service.name') . ' environments as relationships');
+          ->addOption('log', null, InputOption::VALUE_REQUIRED, 'The name of a log file. Defaults to ' . $this->config()->get('local.local_dir') . '/server.log')
+          ->addOption('tunnel', null, InputOption::VALUE_NONE, 'Incorporate SSH tunnels to remote ' . $this->config()->get('service.name') . ' environments as relationships');
     }
 
     public function isEnabled()
@@ -56,7 +56,7 @@ class ServerStartCommand extends ServerCommandBase
         }
 
         $multiApp = count($apps) > 1;
-        $webRoot = $projectRoot . '/' . self::$config->get('local.web_root');
+        $webRoot = $projectRoot . '/' . $this->config()->get('local.web_root');
         $items = [];
         foreach ($apps as $app) {
             $appId = $app->getId();
@@ -86,12 +86,12 @@ class ServerStartCommand extends ServerCommandBase
                 );
                 if ($result != 0) {
                     $this->stdErr->writeln(sprintf('Failed to get SSH tunnel information for the app <error>%s</error>', $appId));
-                    $this->stdErr->writeln('Run <comment>' . self::$config->get('application.executable') . ' tunnel:open</comment> to create tunnels.');
+                    $this->stdErr->writeln('Run <comment>' . $this->config()->get('application.executable') . ' tunnel:open</comment> to create tunnels.');
                     unset($items[$appId]);
                     continue;
                 }
                 $relationships = $bufferedOutput->fetch();
-                $items[$appId]['env'][self::$config->get('application.env_prefix') . 'RELATIONSHIPS'] = $relationships;
+                $items[$appId]['env'][$this->config()->get('service.env_prefix') . 'RELATIONSHIPS'] = $relationships;
             }
         }
 
@@ -99,7 +99,7 @@ class ServerStartCommand extends ServerCommandBase
             return 1;
         }
 
-        $logFile = $input->getOption('log') ?: $projectRoot . '/' . self::$config->get('local.local_dir') . '/server.log';
+        $logFile = $input->getOption('log') ?: $projectRoot . '/' . $this->config()->get('local.local_dir') . '/server.log';
         $log = $this->openLog($logFile);
         if (!$log) {
             $this->stdErr->writeln(sprintf('Failed to open log file for writing: <error>%s</error>', $logFile));
@@ -115,7 +115,6 @@ class ServerStartCommand extends ServerCommandBase
         $processes = [];
         $force = $input->getOption('force');
         foreach ($items as $appId => $item) {
-            $appName = $item['name'];
             $appConfig = $item['config'];
             $address = $item['address'];
             $docRoot = $item['docRoot'];
@@ -123,10 +122,10 @@ class ServerStartCommand extends ServerCommandBase
             if ($otherServer = $this->isServerRunningForApp($appId, $projectRoot)) {
                 if (!$force) {
                     $this->stdErr->writeln(sprintf(
-                      'A server is already running for the app <info>%s</info> at http://%s, PID %s',
-                      $appId,
-                      $otherServer['address'],
-                      $otherServer['pid']
+                        'A server is already running for the app <info>%s</info> at http://%s, PID %s',
+                        $appId,
+                        $otherServer['address'],
+                        $otherServer['pid']
                     ));
                     continue;
                 }
@@ -134,9 +133,10 @@ class ServerStartCommand extends ServerCommandBase
                 // If the port was not manually specified, kill the old server and
                 // take its address.
                 $this->stdErr->writeln(sprintf(
-                  'Stopping server for the app <info>%s</info> at http://%s',
-                  $appId,
-                  $otherServer['address']));
+                    'Stopping server for the app <info>%s</info> at http://%s',
+                    $appId,
+                    $otherServer['address']
+                ));
                 $this->stopServer($address, $otherServer['pid']);
                 sleep(1);
 
@@ -145,13 +145,12 @@ class ServerStartCommand extends ServerCommandBase
                 if (!$input->getOption('port') && $input->getOption('ip') === '127.0.0.1') {
                     $address = $otherServer['address'];
                 }
-            }
-            elseif ($otherPid = $this->isServerRunningForAddress($address)) {
+            } elseif ($otherPid = $this->isServerRunningForAddress($address)) {
                 if (!$force || $otherPid === true) {
                     $this->stdErr->writeln(sprintf(
-                      'A server is already running at address: http://%s, PID %s',
-                      $address,
-                      $otherPid === true ? 'unknown' : $otherPid
+                        'A server is already running at address: http://%s, PID %s',
+                        $address,
+                        $otherPid === true ? 'unknown' : $otherPid
                     ));
                     $error = true;
                     continue;
@@ -167,8 +166,7 @@ class ServerStartCommand extends ServerCommandBase
 
             try {
                 $processManager->startProcess($process, $pidFile, $log);
-            }
-            catch (\Exception $e) {
+            } catch (\Exception $e) {
                 $this->stdErr->writeln(sprintf('Failed to start server: %s', $e->getMessage()));
                 unset($processes[$address]);
                 $error = true;
@@ -199,7 +197,7 @@ class ServerStartCommand extends ServerCommandBase
 
         if (count($processes)) {
             $this->stdErr->writeln(sprintf('Logs are written to: %s', $logFile));
-            $this->stdErr->writeln('Use <comment>' . self::$config->get('application.executable') . ' server:stop</comment> to stop server(s)');
+            $this->stdErr->writeln('Use <comment>' . $this->config()->get('application.executable') . ' server:stop</comment> to stop server(s)');
         }
 
         // The terminal has received all necessary output, so we can stop the