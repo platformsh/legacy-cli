50eb6cd0a60f85f459083f33b9aa1831972e9176
Patrick Dawkins <pjcdawkins@gmail.com>
2017-04-21 12:35:08 +0100

    Remove unnecessary 2>&1 from sql/drush over SSH commands

--- src/Command/Db/DbSqlCommand.php
+++ src/Command/Db/DbSqlCommand.php
@@ -58,7 +58,7 @@ class DbSqlCommand extends CommandBase
 
         $query = $input->getArgument('query');
         if ($query) {
-            $sqlCommand .= $queryOption . escapeshellarg($query) . ' 2>&1';
+            $sqlCommand .= $queryOption . escapeshellarg($query);
         }
 
         /** @var \Platformsh\Cli\Service\Ssh $ssh */
