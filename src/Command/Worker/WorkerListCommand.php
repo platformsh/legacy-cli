49f1224813d6f63b61a384554109a9023d57b021
Patrick Dawkins <pjcdawkins@gmail.com>
2017-12-20 14:01:41 +0000

    Add worker:list command

---
+++ src/Command/Worker/WorkerListCommand.php
@@ -0,0 +1,51 @@
+<?php
+namespace Platformsh\Cli\Command\Worker;
+
+use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Service\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WorkerListCommand extends CommandBase
+{
+    protected $hiddenInList = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('worker:list')
+            ->setAliases(['workers'])
+            ->setDescription('Get a list of all deployed workers');
+        $this->addProjectOption()
+            ->addEnvironmentOption();
+        Table::configureInput($this->getDefinition());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->validateInput($input);
+
+        $workers = $this->getSelectedEnvironment()->getCurrentDeployment()->workers;
+        if (empty($workers)) {
+            $this->stdErr->writeln('No workers found.');
+
+            return 0;
+        }
+
+        $rows = [];
+        foreach ($workers as $worker) {
+            $rows[] = [$worker->name, $worker->type, json_encode($worker->worker)];
+        }
+
+        /** @var \Platformsh\Cli\Service\Table $table */
+        $table = $this->getService('table');
+        $table->render($rows, ['Name', 'Type', 'Worker config']);
+
+        return 0;
+    }
+}
