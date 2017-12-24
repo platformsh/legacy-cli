0a7692129b3ea34258c63700a9258df15c886e71
Patrick Dawkins <pjcdawkins@gmail.com>
2017-12-24 08:56:01 +0000

    Updates to auth:oauth2 according to review
    * Explain sleeping after starting PHP server
    * Update HTML template
    * Add paragonie/random_compat
    * Use a known code file and always clean up.

--- resources/oauth-listener/index.php
+++ resources/oauth-listener/index.php
@@ -1,76 +1,146 @@
 <?php
 
-ob_start();
+namespace Platformsh\Cli\OAuth;
 
-$header = <<<EOF
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>CLI login (temporary window/tab)</title>
-</head>
-<body>
-<h1>CLI login (temporary window/tab)</h1>
-EOF;
-$footer = <<<EOF
-</body>
-</html>
-EOF;
+class Listener
+{
+    private $state;
+    private $accountsUrl;
+    private $clientId;
+    private $file;
+    private $localUrl;
 
-$state = getenv('CLI_OAUTH_STATE');
-$accounts_url = getenv('CLI_OAUTH_ACCOUNTS_URL');
-$client_id = getenv('CLI_OAUTH_CLIENT_ID');
-$file = getenv('CLI_OAUTH_FILE');
-if (!$state || !$accounts_url || !$client_id || !$file) {
-    exit('Invalid environment: ' . print_r($_ENV, true));
-}
-
-if (array_key_exists('done', $_GET)) {
-    ob_end_clean();
-    echo $header;
-    echo <<<EOF
-        <p><strong>Successfully logged in.</strong></p>
-        <p>You can return to the command line.</p>
-        <p>You may also wish to <a href="{$accounts_url}/user/logout">log out</a> in this browser.</p>
-EOF;
-    echo $footer;
-    exit;
-}
-
-$localUrl = 'http://127.0.0.1:' . $_SERVER['SERVER_PORT'];
-
-$url = $accounts_url . '/oauth2/authorize?' . http_build_query([
-    'redirect_uri' => $localUrl,
-    'state' => $state,
-    'client_id' => $client_id,
-    'response_type' => 'code',
-], null, '&', PHP_QUERY_RFC3986);
-
-if (!isset($_GET['state'], $_GET['code'])) {
-    header('Location: ' . $url);
-    exit;
-}
-
-try {
-    if ($_GET['state'] !== $state) {
-        throw new \RuntimeException('Invalid state');
+    public function __construct() {
+        $required = [
+            'CLI_OAUTH_STATE',
+            'CLI_OAUTH_ACCOUNTS_URL',
+            'CLI_OAUTH_CLIENT_ID',
+            'CLI_OAUTH_FILE',
+        ];
+        if ($missing = array_diff($required, array_keys($_ENV))) {
+            throw new \RuntimeException('Invalid environment, missing: ' . implode(', ', $missing));
+        }
+        $this->state = $_ENV['CLI_OAUTH_STATE'];
+        $this->accountsUrl = $_ENV['CLI_OAUTH_ACCOUNTS_URL'];
+        $this->clientId = $_ENV['CLI_OAUTH_CLIENT_ID'];
+        $this->file = $_ENV['CLI_OAUTH_FILE'];
+        $this->localUrl = $localUrl = 'http://127.0.0.1:' . $_SERVER['SERVER_PORT'];
     }
 
-    if (!file_exists($file) || !is_writeable($file)) {
-        throw new \RuntimeException('Failed to find file to save code');
+    /**
+     * @return string
+     */
+    private function getOAuthUrl()
+    {
+        return $this->accountsUrl . '/oauth2/authorize?' . http_build_query([
+            'redirect_uri' => $this->localUrl,
+            'state' => $this->state,
+            'client_id' => $this->clientId,
+            'response_type' => 'code',
+        ], null, '&', PHP_QUERY_RFC3986);
     }
 
-    if (!file_put_contents($file, $_GET['code'])) {
-        throw new \RuntimeException('Failed to write authorization code to file');
+    /**
+     * Check state, run logic, return page content.
+     *
+     * @return string
+     */
+    public function run()
+    {
+        if (array_key_exists('done', $_GET)) {
+            $content = '<p><strong>Successfully logged in.</strong></p>';
+            $content .= '<p>You can return to the command line.</p>';
+
+            // @todo find a way to avoid this
+            $logoutUrl = htmlspecialchars($this->accountsUrl . '/user/logout');
+            $content .= '<p>You may also wish to <a href="' . $logoutUrl . '">log out</a> in this browser.</p>';
+
+            return $content;
+        }
+        if (!isset($_GET['state'], $_GET['code'])) {
+            return $this->redirectToLogin();
+        }
+        if ($_GET['state'] !== $this->state) {
+            return $this->reportError('Invalid state parameter');
+        }
+        if (!file_put_contents($this->file, $_GET['code'], LOCK_EX)) {
+            return $this->reportError('Failed to write authorization code to file');
+        }
+
+        header('Location: ' . $this->localUrl . '/?done');
+
+        return '<p>Logging in, please wait...</p>';
     }
 
-    header('Location: ' . $localUrl . '?done');
-} catch (\RuntimeException $e) {
-    http_response_code(401);
-    echo $header;
-    $message = htmlspecialchars($e->getMessage());
-    echo <<<EOF
+    /**
+     * @return string
+     */
+    private function redirectToLogin()
+    {
+        $url = $this->getOAuthUrl();
+        header('Location: ' . $url);
+
+        return '<p><a href="' . htmlspecialchars($url) .'">Log in</a>.</p>';
+    }
+
+    /**
+     * @return string
+     */
+    private function reportError($message)
+    {
+        http_response_code(401);
+        $message = htmlspecialchars($message);
+
+        return <<<EOF
     <p>An error occurred while trying to log in. Please try again.</p>
     <p>Error message: <code>{$message}</code></p>
 EOF;
-    echo $footer;
+    }
 }
+
+$content = (new Listener())->run();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>CLI Login</title>
+    <style>
+        html {
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            font-weight: 300;
+            background-color: #eee;
+        }
+
+        h1 {
+            font-weight: 100;
+        }
+
+        body {
+            margin: 3em;
+        }
+
+        img {
+            display: block;
+            width: 100px;
+            height: 100px;
+            margin: 0 auto;
+        }
+
+        div {
+            max-width: 20em;
+            margin: 0 auto;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <img
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQMAAABKLAcXAAAABlBMVEUAAADg4ODy8Xj7AAAAAXRSTlMAQObYZgAAAB5JREFUOMtj+I8EPozyRnlU4w1NMJhCcDT+hm2MAQAJBMb6YxK/8wAAAABJRU5ErkJggg=="
+        alt="">
+    <?php echo $content; ?>
+</div>
+</body>
+</html>
--- src/Command/Auth/OAuth2Command.php
+++ src/Command/Auth/OAuth2Command.php
@@ -41,10 +41,16 @@ class OAuth2Command extends CommandBase
             return 0;
         }
 
+        // Set up the local PHP web server, which will serve an OAuth2 redirect
+        // and wait for the response.
+        // Firstly, find an address. The port needs to be within a known range,
+        // for validation by the remote server.
         $port = PortUtil::getPort(5000, null, 5005);
         $localAddress = '127.0.0.1:' . $port;
         $localUrl = 'http://' . $localAddress;
 
+        // Then create the document root for the local server. This needs to be
+        // outside the CLI itself (since the CLI may be run as a Phar).
         $listenerDir = $this->config()->getWritableUserDir() . '/oauth-listener';
         if (!is_dir($listenerDir) && !mkdir($listenerDir, 0700, true)) {
             throw new \RuntimeException('Failed to create temporary directory: ' . $listenerDir);
@@ -53,13 +59,15 @@ class OAuth2Command extends CommandBase
             throw new \RuntimeException('Failed to write temporary file: ' . $listenerDir . '/index.php');
         }
 
-        $tmpFile = tempnam($this->config()->getWritableUserDir(), 'oauth');
-        @chmod($tmpFile, 0600);
-        if (!file_exists($tmpFile) || !realpath($tmpFile)) {
-            throw new \RuntimeException('Failed to create temporary file: ' . $tmpFile);
+        // Create the file where an authorization code will be saved (by the
+        // local server script).
+        $codeFile = $listenerDir . '/.code';
+        if (file_put_contents($codeFile, '', LOCK_EX) === false) {
+            throw new \RuntimeException('Failed to create temporary file: ' . $codeFile);
         }
-        $tmpFile = realpath($tmpFile);
+        chmod($codeFile, 0600);
 
+        // Start the local server.
         $process = new Process([
             'php',
             '-dvariables_order=egps',
@@ -72,71 +80,81 @@ class OAuth2Command extends CommandBase
             'CLI_OAUTH_STATE' => $this->getRandomState(),
             'CLI_OAUTH_ACCOUNTS_URL' => $this->config()->get('service.accounts_url'),
             'CLI_OAUTH_CLIENT_ID' => $this->config()->get('api.oauth2_client_id'),
-            'CLI_OAUTH_FILE' => $tmpFile,
+            'CLI_OAUTH_FILE' => $codeFile,
         ]);
-        $process->setTimeout(300);
-
+        $process->setTimeout(null);
         $process->start();
 
+        // Give the local server some time to start (before checking its status
+        // or opening the browser).
         usleep(500000);
 
+        // Check the local server status.
+        if (!$process->isRunning()) {
+            $this->stdErr->writeln('Failed to start local web server.');
+            $this->stdErr->writeln(trim($process->getErrorOutput()));
+
+            return 1;
+        }
+
+        // Open the local server URL in a browser (or print the URL).
         /** @var \Platformsh\Cli\Service\Url $urlService */
         $urlService = $this->getService('url');
         if ($urlService->openUrl($localUrl, false)) {
-            $this->stdErr->writeln(
-                'Please use the browser to log in using your <info>' . $this->config()->get('service.name') . '</info> account.'
-            );
+            $this->stdErr->writeln('Please use the browser to log in.');
         } else {
-            $this->stdErr->writeln(
-                'Open the following URL and log in using your <info>' . $this->config()->get('service.name') . '</info> account:'
-            );
+            $this->stdErr->writeln('Open the following URL and log in:');
             $this->stdErr->writeln($localUrl);
         }
 
+        // Wait for the file to be filled with an OAuth2 authorization code.
         $code = null;
         while ($process->isRunning() && empty($code)) {
             usleep(300000);
-            if (!file_exists($tmpFile)) {
-                throw new \RuntimeException('File not found while waiting for authorization code: ' . $tmpFile);
+            if (!file_exists($codeFile)) {
+                $this->stdErr->writeln('File not found: <error>' . $codeFile . '</error>');
+                break;
             }
-            $contents = file_get_contents($tmpFile);
-            if ($contents === false) {
-                unlink($tmpFile);
-                throw new \RuntimeException('Failed to read file while waiting for authorization code');
+            $code = file_get_contents($codeFile);
+            if ($code === false) {
+                $this->stdErr->writeln('Failed to read file: <error>' . $codeFile . '</error>');
+                break;
             }
-            $code = trim($contents);
         }
 
         // Clean up.
-        // @todo make this more reliable
-        unlink($tmpFile);
+        unlink($codeFile);
         unlink($listenerDir . '/index.php');
         rmdir($listenerDir);
 
+        $this->stdErr->writeln('');
+
         if (empty($code)) {
-            throw new \RuntimeException('Failed to get authorization code');
+            $this->stdErr->writeln('Failed to get an authorization code. Please try again.');
+
+            return 1;
         }
 
-        $this->stdErr->writeln('');
-        $this->stdErr->writeln('Login information received. Verifying...');
-
-        $this->saveAccessToken(
-            $this->getAccessToken($code, $localUrl)
-        );
-
+        // Clear the cache (as we are about to log in).
         /** @var \Doctrine\Common\Cache\CacheProvider $cache */
         $cache = $this->getService('cache');
         $cache->flushAll();
 
+        // Using the authorization code, request an access token, and save it
+        // to the session.
+        $this->stdErr->writeln('Login information received. Verifying...');
+        $this->saveAccessToken(
+            $this->getAccessToken($code, $localUrl)
+        );
+
+        // Report success, with user account info.
         $info = $this->api()->getClient(false)->getAccountInfo();
-        if (isset($info['username'], $info['mail'])) {
-            $this->stdErr->writeln('');
-            $this->stdErr->writeln(sprintf(
-                'You are logged in as <info>%s</info> (%s).',
-                $info['username'],
-                $info['mail']
-            ));
-        }
+        $this->stdErr->writeln('');
+        $this->stdErr->writeln(sprintf(
+            'You are logged in as <info>%s</info> (%s).',
+            $info['username'],
+            $info['mail']
+        ));
 
         return 0;
     }
@@ -184,19 +202,13 @@ class OAuth2Command extends CommandBase
     }
 
     /**
+     * Get a random state to use with the OAuth2 code request.
+     *
      * @return string
      */
     private function getRandomState()
     {
-        if (function_exists('random_bytes')) {
-            $state = random_bytes(128);
-        } else {
-            $state = openssl_random_pseudo_bytes(128, $strong);
-            if (!$strong) {
-                throw new \RuntimeException('Failed to calculate strong random state');
-            }
-        }
-
-        return bin2hex($state);
+        // This uses paragonie/random_compat as a polyfill for PHP < 7.0.
+        return bin2hex(random_bytes(128));
     }
 }
