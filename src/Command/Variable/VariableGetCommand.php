71c2ecc0777b50e4f98680f04cd4955e8f7f043d
Patrick Dawkins <pjcdawkins@gmail.com>
2018-03-04 23:00:58 +0000

    Modify variable:get so it works with project or environment level variables

--- src/Command/Variable/VariableGetCommand.php
+++ src/Command/Variable/VariableGetCommand.php
@@ -1,16 +1,14 @@
 <?php
 namespace Platformsh\Cli\Command\Variable;
 
-use Platformsh\Cli\Command\CommandBase;
-use Platformsh\Cli\Console\AdaptiveTableCell;
 use Platformsh\Cli\Service\Table;
-use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Platformsh\Client\Model\Variable as EnvironmentLevelVariable;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class VariableGetCommand extends CommandBase
+class VariableGetCommand extends VariableCommandBase
 {
     /**
      * {@inheritdoc}
@@ -20,9 +18,9 @@ class VariableGetCommand extends CommandBase
         $this
             ->setName('variable:get')
             ->setAliases(['variables', 'vget'])
-            ->addArgument('name', InputArgument::OPTIONAL, 'The name of the variable')
-            ->addOption('pipe', null, InputOption::VALUE_NONE, 'Output the full variable value only (a "name" must be specified)')
-            ->setDescription('View variable(s) for an environment');
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the variable')
+            ->addOption('pipe', null, InputOption::VALUE_NONE, 'Output the full variable value only')
+            ->setDescription('View a variable');
         Table::configureInput($this->getDefinition());
         $this->addProjectOption()
              ->addEnvironmentOption();
@@ -32,63 +30,31 @@ class VariableGetCommand extends CommandBase
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateInput($input);
+        $this->validateInput($input, true);
 
-        if ($name = $input->getArgument('name')) {
-            $variable = $this->getSelectedEnvironment()
-                             ->getVariable($name);
-            if (!$variable) {
-                $this->stdErr->writeln("Variable not found: <error>$name</error>");
+        $name = $input->getArgument('name');
 
-                return 1;
-            }
-
-            if ($input->getOption('pipe')) {
-                $output->writeln($variable->value);
-            } else {
-                $output->writeln(sprintf('<info>%s</info>: %s', $variable->name, $variable->value));
-            }
-
-            if (!$variable->is_enabled) {
-                $this->stdErr->writeln(sprintf(
-                    "The variable <comment>%s</comment> is disabled.\nEnable it with: <comment>%s variable:enable %s</comment>",
-                    $variable->name,
-                    $this->config()->get('application.executable'),
-                    escapeshellarg($variable->name)
-                ));
-            }
-
-            return 0;
-        }
-
-        $results = $this->getSelectedEnvironment()
-                        ->getVariables();
-        if (!$results) {
-            $this->stdErr->writeln('No variables found');
+        $variable = $this->getExistingVariable($name);
+        if (!$variable) {
+            $this->stdErr->writeln("Variable not found: <error>$name</error>");
 
             return 1;
         }
 
         if ($input->getOption('pipe')) {
-            throw new InvalidArgumentException('Specify a variable name to use --pipe');
+            $output->writeln($variable->value);
+        } else {
+            $output->writeln(sprintf('<info>%s</info>: %s', $variable->name, $variable->value));
         }
 
-        /** @var \Platformsh\Cli\Service\Table $table */
-        $table = $this->getService('table');
-
-        $header = ['ID', 'Value', 'Enabled', 'Inherited', 'JSON'];
-        $rows = [];
-        foreach ($results as $variable) {
-            $rows[] = [
-                new AdaptiveTableCell($variable->id, ['wrap' => false]),
-                $variable->value,
-                $variable->is_enabled ? 'Yes' : 'No',
-                $variable->inherited ? 'Yes' : 'No',
-                $variable->is_json ? 'Yes' : 'No',
-            ];
+        if ($variable instanceof EnvironmentLevelVariable && !$variable->is_enabled) {
+            $this->stdErr->writeln(sprintf(
+                "The variable <comment>%s</comment> is disabled.\nEnable it with: <comment>%s variable:enable %s</comment>",
+                $variable->name,
+                $this->config()->get('application.executable'),
+                escapeshellarg($variable->name)
+            ));
         }
-
-        $table->render($rows, $header);
 
         return 0;
     }
