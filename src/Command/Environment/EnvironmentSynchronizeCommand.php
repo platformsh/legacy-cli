83ed1909a9e24dfea36e30bc241fdec0dff91741
Patrick Dawkins <pjcdawkins@gmail.com>
2018-05-10 19:26:08 +0100

    Add --rebase option to the environment:synchronize (sync) command.

--- src/Command/Environment/EnvironmentSynchronizeCommand.php
+++ src/Command/Environment/EnvironmentSynchronizeCommand.php
@@ -6,6 +6,7 @@ use Stecman\Component\Symfony\Console\BashCompletion\Completion\CompletionAwareI
 use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class EnvironmentSynchronizeCommand extends CommandBase implements CompletionAwareInterface
@@ -17,7 +18,8 @@ class EnvironmentSynchronizeCommand extends CommandBase implements CompletionAwa
             ->setName('environment:synchronize')
             ->setAliases(['sync'])
             ->setDescription("Synchronize an environment's code and/or data from its parent")
-            ->addArgument('synchronize', InputArgument::IS_ARRAY, 'What to synchronize: "code", "data" or both');
+            ->addArgument('synchronize', InputArgument::IS_ARRAY, 'What to synchronize: "code", "data" or both')
+            ->addOption('rebase', null, InputOption::VALUE_NONE, 'Synchronize code by rebasing instead of merging');
         $this->addProjectOption()
              ->addEnvironmentOption()
              ->addWaitOptions();
@@ -57,6 +59,8 @@ EOT
         /** @var \Platformsh\Cli\Service\QuestionHelper $questionHelper */
         $questionHelper = $this->getService('question_helper');
 
+        $rebase = (bool) $input->getOption('rebase');
+
         if ($synchronize = $input->getArgument('synchronize')) {
             // The input was invalid.
             if (array_diff($input->getArgument('synchronize'), ['code', 'data', 'both'])) {
@@ -65,8 +69,19 @@ EOT
             }
             $syncCode = in_array('code', $synchronize) || in_array('both', $synchronize);
             $syncData = in_array('data', $synchronize) || in_array('both', $synchronize);
+
+            if ($rebase && !$syncCode) {
+                $this->stdErr->writeln('<comment>Note:</comment> you specified the <comment>--rebase</comment> option, but this only applies to synchronizing code, which you have not selected.');
+                $this->stdErr->writeln('');
+            }
+
+            $toSync = $syncCode && $syncData
+                ? '<options=underscore>code</> and <options=underscore>data</>'
+                : '<options=underscore>' . ($syncCode ? 'code' : 'data') . '</>';
+
             $confirmText = sprintf(
-                'Are you sure you want to synchronize <info>%s</info> to <info>%s</info>?',
+                'Are you sure you want to synchronize %s from <info>%s</info> to <info>%s</info>?',
+                $toSync,
                 $parentId,
                 $environmentId
             );
@@ -75,23 +90,38 @@ EOT
             }
         } else {
             $syncCode = $questionHelper->confirm(
-                "Synchronize code from <info>$parentId</info> to <info>$environmentId</info>?",
+                "Do you want to synchronize <options=underscore>code</> from <info>$parentId</info> to <info>$environmentId</info>?",
                 false
             );
+
+            if ($syncCode && !$rebase) {
+                $rebase = $questionHelper->confirm(
+                    "Do you want to synchronize code by rebasing instead of merging?",
+                    false
+                );
+            }
+
+            if ($rebase && !$syncCode) {
+                $this->stdErr->writeln('<comment>Note:</comment> you specified the <comment>--rebase</comment> option, but you have not selected to synchronize code. Only data will be synchronized.');
+            }
+
+            $this->stdErr->writeln('');
+
             $syncData = $questionHelper->confirm(
-                "Synchronize data from <info>$parentId</info> to <info>$environmentId</info>?",
+                "Do you want to synchronize <options=underscore>data</> from <info>$parentId</info> to <info>$environmentId</info>?",
                 false
             );
         }
         if (!$syncCode && !$syncData) {
-            $this->stdErr->writeln("<error>You must synchronize at least code or data.</error>");
+            $this->stdErr->writeln('');
+            $this->stdErr->writeln('You did not select anything to synchronize.');
 
             return 1;
         }
 
         $this->stdErr->writeln("Synchronizing environment <info>$environmentId</info>");
 
-        $activity = $selectedEnvironment->synchronize($syncData, $syncCode);
+        $activity = $selectedEnvironment->synchronize($syncData, $syncCode, $rebase);
         if ($this->shouldWait($input)) {
             /** @var \Platformsh\Cli\Service\ActivityMonitor $activityMonitor */
             $activityMonitor = $this->getService('activity_monitor');
