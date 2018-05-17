platformsh/legacy-cli

commit 6659d65b3e6057a23a0df43a482e013e9145bc10

Clear environment/project caches after a 403 error

src/Service/Api.php
@@ -4,6 +4,7 @@ namespace Platformsh\Cli\Service;
 
 use Doctrine\Common\Cache\CacheProvider;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Event\ErrorEvent;
 use Platformsh\Cli\Event\EnvironmentsChangedEvent;
 use Platformsh\Cli\Exception\ApiFeatureMissingException;
 use Platformsh\Cli\Session\KeychainStorage;
@@ -209,6 +210,12 @@ class Api
             $session->setStorage($this->sessionStorage);
 
             self::$client = new PlatformClient($connector);
+
+            $connector->getClient()->getEmitter()->on('error', function (ErrorEvent $event) {
+                if ($event->getResponse() && $event->getResponse()->getStatusCode() === 403) {
+                    $this->on403($event);
+                }
+            });
 
             if ($autoLogin && !$connector->isLoggedIn()) {
                 $this->dispatcher->dispatch('login_required');
@@ -886,5 +893,26 @@ class Api
         $environment = $this->getEnvironment($environment->id, $this->getProject($environment->project), $refresh);
 
         return $environment->operationAvailable($op);
+    }
+
+    /**
+     * React on an API 403 request.
+     *
+     * @param \GuzzleHttp\Event\ErrorEvent $event
+     */
+    private function on403(ErrorEvent $event)
+    {
+        $url = $event->getRequest()->getUrl();
+        $path = parse_url($url, PHP_URL_PATH);
+        if ($path && strpos($path, '/api/projects/') === 0) {
+            // Clear the environments cache for environment request errors.
+            if (preg_match('#^/api/projects/([^/]+?)/environments/#', $path, $matches)) {
+                $this->clearEnvironmentsCache($matches[1]);
+            }
+            // Clear the projects cache for other project request errors.
+            if (preg_match('#^/api/projects/([^/]+?)[/$]/#', $path, $matches)) {
+                $this->clearProjectsCache();
+            }
+        }
     }
 }

commit 8739d4b20df676a2f012d30018527db8645983a7

Work around regression caused by previous commit 6659d65
Addresses #717

src/Service/Api.php
@@ -211,14 +211,18 @@ class Api
 
             self::$client = new PlatformClient($connector);
 
-            $connector->getClient()->getEmitter()->on('error', function (ErrorEvent $event) {
-                if ($event->getResponse() && $event->getResponse()->getStatusCode() === 403) {
-                    $this->on403($event);
-                }
-            });
-
             if ($autoLogin && !$connector->isLoggedIn()) {
                 $this->dispatcher->dispatch('login_required');
+            }
+
+            try {
+                $connector->getClient()->getEmitter()->on('error', function (ErrorEvent $event) {
+                    if ($event->getResponse() && $event->getResponse()->getStatusCode() === 403) {
+                        $this->on403($event);
+                    }
+                });
+            } catch (\RuntimeException $e) {
+                // Ignore errors if the user is not logged in at this stage.
             }
         }
 