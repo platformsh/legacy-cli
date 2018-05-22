8498df346261a87f3e2e298f9287f6832f5555be
Patrick Dawkins <pjcdawkins@gmail.com>
2018-05-22 09:34:07 +0100

    Add DI to environment:redeploy

--- src/Command/Environment/EnvironmentRedeployCommand.php
+++ src/Command/Environment/EnvironmentRedeployCommand.php
@@ -2,41 +2,50 @@
 namespace Platformsh\Cli\Command\Environment;
 
 use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Service\ActivityMonitor;
+use Platformsh\Cli\Service\QuestionHelper;
+use Platformsh\Cli\Service\Selector;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class EnvironmentRedeployCommand extends CommandBase
 {
-
     protected static $defaultName = 'environment:redeploy';
+
+    private $activityMonitor;
+    private $questionHelper;
+    private $selector;
+
+    public function __construct(ActivityMonitor $activityMonitor, QuestionHelper $questionHelper, Selector $selector)
+    {
+        $this->activityMonitor = $activityMonitor;
+        $this->questionHelper = $questionHelper;
+        $this->selector = $selector;
+        parent::__construct();
+    }
 
     protected function configure()
     {
         $this->setAliases(['redeploy'])
             ->setDescription('Redeploy an environment');
-        $this->addProjectOption()
-            ->addEnvironmentOption();
-        $this->addWaitOptions();
+        $definition = $this->getDefinition();
+        $this->selector->addProjectOption($definition);
+        $this->selector->addEnvironmentOption($definition);
+        $this->activityMonitor->addWaitOptions($definition);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateInput($input);
+        $environment = $this->selector->getSelection($input)->getEnvironment();
 
-        $environment = $this->getSelectedEnvironment();
-
-        /** @var \Platformsh\Cli\Service\QuestionHelper $questionHelper */
-        $questionHelper = $this->getService('question_helper');
-        if (!$questionHelper->confirm('Are you sure you want to redeploy the environment <comment>' . $environment->id . '</comment>?')) {
+        if (!$this->questionHelper->confirm('Are you sure you want to redeploy the environment <comment>' . $environment->id . '</comment>?')) {
             return 1;
         }
 
         $activity = $environment->redeploy();
 
-        if ($this->shouldWait($input)) {
-            /** @var \Platformsh\Cli\Service\ActivityMonitor $activityMonitor */
-            $activityMonitor = $this->getService('activity_monitor');
-            $success = $activityMonitor->waitAndLog($activity);
+        if ($this->activityMonitor->shouldWait($input)) {
+            $success = $this->activityMonitor->waitAndLog($activity);
             if (!$success) {
                 return 1;
             }
