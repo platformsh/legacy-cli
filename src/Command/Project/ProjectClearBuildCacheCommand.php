03f828b6ab5072c9709ac964b175b47371586f00
Patrick Dawkins <pjcdawkins@gmail.com>
2018-05-23 21:25:07 +0100

    Add DI to project:clear-build-cache command

--- src/Command/Project/ProjectClearBuildCacheCommand.php
+++ src/Command/Project/ProjectClearBuildCacheCommand.php
@@ -2,6 +2,8 @@
 namespace Platformsh\Cli\Command\Project;
 
 use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Service\Api;
+use Platformsh\Cli\Service\Selector;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -9,18 +11,26 @@ class ProjectClearBuildCacheCommand extends CommandBase
 {
     protected static $defaultName = 'project:clear-build-cache';
 
+    private $api;
+    private $selector;
+
+    public function __construct(Api $api, Selector $selector) {
+        $this->api = $api;
+        $this->selector = $selector;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this->setDescription("Clear a project's build cache");
-        $this->addProjectOption();
+        $this->selector->addProjectOption($this->getDefinition());
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateInput($input);
-        $project = $this->getSelectedProject();
+        $project = $this->selector->getSelection($input)->getProject();
         $project->clearBuildCache();
-        $this->stdErr->writeln('The build cache has been cleared on the project: ' . $this->api()->getProjectLabel($project));
+        $this->stdErr->writeln('The build cache has been cleared on the project: ' . $this->api->getProjectLabel($project));
 
         return 0;
     }
