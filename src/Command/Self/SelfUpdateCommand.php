5cc61bc3a0ba342eb9c640b513d8f2d03655e98b
Patrick Dawkins <pjcdawkins@gmail.com>
2018-05-23 23:49:29 +0100

    Add DI to self:update command

--- src/Command/Self/SelfUpdateCommand.php
+++ src/Command/Self/SelfUpdateCommand.php
@@ -2,6 +2,8 @@
 namespace Platformsh\Cli\Command\Self;
 
 use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Service\Config;
+use Platformsh\Cli\Service\SelfUpdater;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,6 +11,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 class SelfUpdateCommand extends CommandBase
 {
     protected static $defaultName = 'self:update';
+
+    private $config;
+    private $updater;
+
+    public function __construct(Config $config, SelfUpdater $updater)
+    {
+        $this->config = $config;
+        $this->updater = $updater;
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -24,16 +36,14 @@ class SelfUpdateCommand extends CommandBase
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $manifestUrl = $input->getOption('manifest') ?: $this->config()->get('application.manifest_url');
-        $currentVersion = $input->getOption('current-version') ?: $this->config()->get('application.version');
+        $manifestUrl = $input->getOption('manifest') ?: $this->config->get('application.manifest_url');
+        $currentVersion = $input->getOption('current-version') ?: $this->config->get('application.version');
 
-        /** @var \Platformsh\Cli\Service\SelfUpdater $cliUpdater */
-        $cliUpdater = $this->getService('self_updater');
-        $cliUpdater->setAllowMajor(!$input->getOption('no-major'));
-        $cliUpdater->setAllowUnstable((bool) $input->getOption('unstable'));
-        $cliUpdater->setTimeout($input->getOption('timeout'));
+        $this->updater->setAllowMajor(!$input->getOption('no-major'));
+        $this->updater->setAllowUnstable((bool) $input->getOption('unstable'));
+        $this->updater->setTimeout($input->getOption('timeout'));
 
-        $result = $cliUpdater->update($manifestUrl, $currentVersion);
+        $result = $this->updater->update($manifestUrl, $currentVersion);
         if ($result === false) {
             return 1;
         }
