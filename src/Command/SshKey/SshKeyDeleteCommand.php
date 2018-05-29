f16856a762fef5e46f9ae86d29b6694a23ac28fe
Patrick Dawkins <pjcdawkins@gmail.com>
2018-05-29 23:04:42 +0100

    Add DI to ssh-key: commands

--- src/Command/SshKey/SshKeyDeleteCommand.php
+++ src/Command/SshKey/SshKeyDeleteCommand.php
@@ -2,6 +2,8 @@
 namespace Platformsh\Cli\Command\SshKey;
 
 use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Service\Api;
+use Platformsh\Cli\Service\Config;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,6 +11,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 class SshKeyDeleteCommand extends CommandBase
 {
     protected static $defaultName = 'ssh-key:delete';
+
+    private $api;
+    private $config;
+
+    public function __construct(Api $api, Config $config)
+    {
+        $this->api = $api;
+        $this->config = $config;
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -27,13 +39,13 @@ class SshKeyDeleteCommand extends CommandBase
         if (empty($id) || !is_numeric($id)) {
             $this->stdErr->writeln('<error>You must specify the ID of the SSH key to delete.</error>');
             $this->stdErr->writeln(
-                'List your SSH keys with: <info>' . $this->config()->get('application.executable') . ' ssh-keys</info>'
+                'List your SSH keys with: <info>' . $this->config->get('application.executable') . ' ssh-keys</info>'
             );
 
             return 1;
         }
 
-        $key = $this->api()->getClient()
+        $key = $this->api->getClient()
                     ->getSshKey($id);
         if (!$key) {
             $this->stdErr->writeln("SSH key not found: <error>$id</error>");
@@ -46,7 +58,7 @@ class SshKeyDeleteCommand extends CommandBase
         $this->stdErr->writeln(sprintf(
             'The SSH key <info>%s</info> has been deleted from your %s account.',
             $id,
-            $this->config()->get('service.name')
+            $this->config->get('service.name')
         ));
 
         return 0;
--- src/Command/SshKey/SshKeyListCommand.php
+++ src/Command/SshKey/SshKeyListCommand.php
@@ -2,6 +2,8 @@
 namespace Platformsh\Cli\Command\SshKey;
 
 use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Service\Api;
+use Platformsh\Cli\Service\Config;
 use Platformsh\Cli\Service\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -10,22 +12,34 @@ class SshKeyListCommand extends CommandBase
 {
     protected static $defaultName = 'ssh-key:list';
 
+    private $api;
+    private $config;
+    private $table;
+
+    public function __construct(Api $api, Config $config, Table $table)
+    {
+        $this->api = $api;
+        $this->config = $config;
+        $this->table = $table;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this->setAliases(['ssh-keys'])
             ->setDescription('Get a list of SSH keys in your account');
-        Table::configureInput($this->getDefinition());
+        $this->table->configureInput($this->getDefinition());
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $keys = $this->api()->getClient()
+        $keys = $this->api->getClient()
                      ->getSshKeys();
 
         if (empty($keys)) {
             $this->stdErr->writeln(sprintf(
                 'You do not yet have any SSH public keys in your %s account.',
-                $this->config()->get('service.name')
+                $this->config->get('service.name')
             ));
         } else {
             /** @var \Platformsh\Cli\Service\Table $table */
@@ -47,7 +61,7 @@ class SshKeyListCommand extends CommandBase
 
         $this->stdErr->writeln('');
 
-        $executable = $this->config()->get('application.executable');
+        $executable = $this->config->get('application.executable');
         $this->stdErr->writeln("Add a new SSH key with: <info>$executable ssh-key:add</info>");
         $this->stdErr->writeln("Delete an SSH key with: <info>$executable ssh-key:delete [id]</info>");
 
