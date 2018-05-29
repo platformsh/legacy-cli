85ec38d11ab59dcd4d8b2f1e663361dd6ae701da
Patrick Dawkins <pjcdawkins@gmail.com>
2018-05-29 21:33:24 +0100

    Fix DI for web command

--- src/Command/WebCommand.php
+++ src/Command/WebCommand.php
@@ -2,6 +2,7 @@
 
 namespace Platformsh\Cli\Command;
 
+use Platformsh\Cli\Service\Api;
 use Platformsh\Cli\Service\Config;
 use Platformsh\Cli\Service\Selector;
 use Platformsh\Cli\Service\Url;
@@ -12,15 +13,18 @@ class WebCommand extends CommandBase
 {
     protected static $defaultName = 'web';
 
+    private $api;
     private $config;
     private $selector;
     private $urlService;
 
     public function __construct(
+        Api $api,
         Config $config,
         Selector $selector,
         Url $urlService
     ) {
+        $this->api = $api;
         $this->config = $config;
         $this->selector = $selector;
         $this->urlService = $urlService;
@@ -39,24 +43,22 @@ class WebCommand extends CommandBase
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Attempt to select the appropriate project and environment.
-        try {
-            $selection = $this->selector->getSelection($input);
-            $environmentId = $selection->getEnvironment()->id;
-        } catch (\Exception $e) {
-            // If a project has been specified but is not found, then error out.
-            if ($input->getOption('project')) {
-                throw $e;
-            }
+        // If an environment ID has been specified but not found, then use
+        // the specified ID anyway. This allows building a URL when an
+        // environment doesn't yet exist.
+        $environmentId = $input->getOption('environment');
+        $input->setOption('environment', null);
 
-            // If an environment ID has been specified but not found, then use
-            // the specified ID anyway. This allows building a URL when an
-            // environment doesn't yet exist.
-            $environmentId = $input->getOption('environment');
+        $selection = $this->selector->getSelection($input, true);
+        $project = $selection->getProject();
+
+        if ($environmentId !== null
+            && ($environment = $this->api->getEnvironment($environmentId, $project, null, true))) {
+            $environmentId = $environment->id;
         }
 
-        $url = $selection->getProject()->getLink('#ui');
-        if ($selection->hasEnvironment()) {
+        $url = $project->getLink('#ui');
+        if ($environmentId !== null) {
             $url .= '/environments/' . rawurlencode($environmentId);
         }
 
