5f27700e98dd353f74d64a8c31fdc380fe98c9c2
Patrick Dawkins <pjcdawkins@gmail.com>
2018-05-30 11:01:26 +0100

    Add DI to service: commands

--- src/Command/Service/MongoDB/MongoDumpCommand.php
+++ src/Command/Service/MongoDB/MongoDumpCommand.php
@@ -3,7 +3,11 @@
 namespace Platformsh\Cli\Command\Service\MongoDB;
 
 use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Service\Git;
+use Platformsh\Cli\Service\QuestionHelper;
 use Platformsh\Cli\Service\Relationships;
+use Platformsh\Cli\Service\Selector;
+use Platformsh\Cli\Service\Shell;
 use Platformsh\Cli\Service\Ssh;
 use Platformsh\Cli\Util\OsUtil;
 use Platformsh\Client\Model\Environment;
@@ -14,45 +18,68 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MongoDumpCommand extends CommandBase
 {
+    protected static $defaultName = 'service:mongo:dump';
+
+    private $git;
+    private $questionHelper;
+    private $relationships;
+    private $selector;
+    private $shell;
+    private $ssh;
+
+    public function __construct(
+        Git $git,
+        QuestionHelper $questionHelper,
+        Relationships $relationships,
+        Selector $selector,
+        Shell $shell,
+        Ssh $ssh
+    ) {
+        $this->git = $git;
+        $this->questionHelper = $questionHelper;
+        $this->relationships = $relationships;
+        $this->selector = $selector;
+        $this->shell = $shell;
+        $this->ssh = $ssh;
+        parent::__construct();
+    }
+
     protected function configure()
     {
-        $this->setName('service:mongo:dump');
         $this->setAliases(['mongodump']);
         $this->setDescription('Create a binary archive dump of data from MongoDB');
         $this->setHidden(true);
         $this->addOption('collection', 'c', InputOption::VALUE_REQUIRED, 'The collection to dump');
         $this->addOption('gzip', 'z', InputOption::VALUE_NONE, 'Compress the dump using gzip');
         $this->addOption('stdout', 'o', InputOption::VALUE_NONE, 'Output to STDOUT instead of a file');
-        Relationships::configureInput($this->getDefinition());
-        Ssh::configureInput($this->getDefinition());
-        $this->addProjectOption()
-            ->addEnvironmentOption()
-            ->addAppOption();
+
+        $definition = $this->getDefinition();
+        $this->relationships->configureInput($definition);
+        $this->ssh->configureInput($definition);
+        $this->selector->addAllOptions($definition);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateInput($input);
-        $projectRoot = $this->getProjectRoot();
+        $selection = $this->selector->getSelection($input);
+        $projectRoot = $this->selector->getProjectRoot();
 
         $gzip = $input->getOption('gzip');
 
-        $appName = $this->selectApp($input);
-        $sshUrl = $this->getSelectedEnvironment()
+        $appName = $selection->getAppName();
+        $sshUrl = $selection->getEnvironment()
             ->getSshUrl($appName);
 
         $dumpFile = false;
 
         if (!$input->getOption('stdout')) {
-            $defaultFilename = $this->getDefaultFilename($this->getSelectedEnvironment(), $appName, $input->getOption('collection'), $gzip);
+            $defaultFilename = $this->getDefaultFilename($selection->getEnvironment(), $appName, $input->getOption('collection'), $gzip);
             $dumpFile = $projectRoot ? $projectRoot . '/' . $defaultFilename : $defaultFilename;
         }
 
         if ($dumpFile) {
             if (file_exists($dumpFile)) {
-                /** @var \Platformsh\Cli\Service\QuestionHelper $questionHelper */
-                $questionHelper = $this->getService('question_helper');
-                if (!$questionHelper->confirm("File exists: <comment>$dumpFile</comment>. Overwrite?", false)) {
+                if (!$this->questionHelper->confirm("File exists: <comment>$dumpFile</comment>. Overwrite?", false)) {
                     return 1;
                 }
             }
@@ -63,14 +90,12 @@ class MongoDumpCommand extends CommandBase
             ));
         }
 
-        /** @var \Platformsh\Cli\Service\Relationships $relationshipsService */
-        $relationshipsService = $this->getService('relationships');
-        $service = $relationshipsService->chooseService($sshUrl, $input, $output, ['mongodb']);
+        $service = $this->relationships->chooseService($sshUrl, $input, $output, ['mongodb']);
         if (!$service) {
             return 1;
         }
 
-        $command = 'mongodump ' . $relationshipsService->getDbCommandArgs('mongodump', $service);
+        $command = 'mongodump ' . $this->relationships->getDbCommandArgs('mongodump', $service);
 
         if ($input->getOption('collection')) {
             $command .= ' --collection ' . OsUtil::escapePosixShellArg($input->getOption('collection'));
@@ -84,9 +109,7 @@ class MongoDumpCommand extends CommandBase
 
         set_time_limit(0);
 
-        /** @var \Platformsh\Cli\Service\Ssh $ssh */
-        $ssh = $this->getService('ssh');
-        $sshCommand = $ssh->getSshCommand();
+        $sshCommand = $this->ssh->getSshCommand();
         $sshCommand .= ' ' . escapeshellarg($sshUrl);
 
         if ($gzip) {
@@ -97,9 +120,6 @@ class MongoDumpCommand extends CommandBase
             $sshCommand .= ' -C';
         }
 
-        /** @var \Platformsh\Cli\Service\Shell $shell */
-        $shell = $this->getService('shell');
-
         $sshCommand .= ' ' . escapeshellarg($command);
 
         if ($dumpFile) {
@@ -107,7 +127,7 @@ class MongoDumpCommand extends CommandBase
         }
 
         $start = microtime(true);
-        $exitCode = $shell->executeSimple($sshCommand);
+        $exitCode = $this->shell->executeSimple($sshCommand);
 
         if ($exitCode === 0 && $output->isVerbose()) {
             $this->stdErr->writeln("\n" . 'The dump completed successfully');
@@ -121,9 +141,7 @@ class MongoDumpCommand extends CommandBase
         // If a dump file exists, check that it's excluded in the project's
         // .gitignore configuration.
         if ($dumpFile && file_exists($dumpFile) && $projectRoot && strpos($dumpFile, $projectRoot) === 0) {
-            /** @var \Platformsh\Cli\Service\Git $git */
-            $git = $this->getService('git');
-            if (!$git->checkIgnore($dumpFile, $projectRoot)) {
+            if (!$this->git->checkIgnore($dumpFile, $projectRoot)) {
                 $this->stdErr->writeln('<comment>Warning: the dump file is not excluded by Git</comment>');
                 if ($pos = strrpos($dumpFile, '.bson')) {
                     $extension = substr($dumpFile, $pos);
--- src/Command/Service/RedisCliCommand.php
+++ src/Command/Service/RedisCliCommand.php
@@ -4,6 +4,8 @@ namespace Platformsh\Cli\Command\Service;
 
 use Platformsh\Cli\Command\CommandBase;
 use Platformsh\Cli\Service\Relationships;
+use Platformsh\Cli\Service\Selector;
+use Platformsh\Cli\Service\Shell;
 use Platformsh\Cli\Service\Ssh;
 use Platformsh\Cli\Util\OsUtil;
 use Symfony\Component\Console\Input\InputArgument;
@@ -12,18 +14,38 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class RedisCliCommand extends CommandBase
 {
+    protected static $defaultName = 'service:redis-cli';
+
+    private $relationships;
+    private $selector;
+    private $shell;
+    private $ssh;
+
+    public function __construct(
+        Relationships $relationships,
+        Selector $selector,
+        Shell $shell,
+        Ssh $ssh
+    ) {
+        $this->relationships = $relationships;
+        $this->selector = $selector;
+        $this->shell = $shell;
+        $this->ssh = $ssh;
+        parent::__construct();
+    }
+
     protected function configure()
     {
-        $this->setName('service:redis-cli');
         $this->setAliases(['redis']);
         $this->setDescription('Access the Redis CLI');
         $this->setHidden(true);
         $this->addArgument('args', InputArgument::OPTIONAL, 'Arguments to add to the Redis command');
-        Relationships::configureInput($this->getDefinition());
-        Ssh::configureInput($this->getDefinition());
-        $this->addProjectOption()
-            ->addEnvironmentOption()
-            ->addAppOption();
+
+        $definition = $this->getDefinition();
+        $this->relationships->configureInput($definition);
+        $this->ssh->configureInput($definition);
+        $this->selector->addAllOptions($definition);
+
         $this->addExample('Open the redis-cli shell');
         $this->addExample('Ping the Redis server', 'ping');
         $this->addExample('Show Redis status information', 'info');
@@ -33,17 +55,15 @@ class RedisCliCommand extends CommandBase
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateInput($input);
+        $selection = $this->selector->getSelection($input);
         if ($this->runningViaMulti && !$input->getArgument('args')) {
             throw new \RuntimeException('The redis-cli command cannot run as a shell via multi');
         }
 
-        $sshUrl = $this->getSelectedEnvironment()
-            ->getSshUrl($this->selectApp($input));
+        $sshUrl = $selection->getEnvironment()
+            ->getSshUrl($selection->getAppName());
 
-        /** @var \Platformsh\Cli\Service\Relationships $relationshipsService */
-        $relationshipsService = $this->getService('relationships');
-        $service = $relationshipsService->chooseService($sshUrl, $input, $output, ['redis']);
+        $service = $this->relationships->chooseService($sshUrl, $input, $output, ['redis']);
         if (!$service) {
             return 1;
         }
@@ -57,24 +77,18 @@ class RedisCliCommand extends CommandBase
             $redisCommand .= ' ' . $args;
         }
 
-        /** @var \Platformsh\Cli\Service\Ssh $ssh */
-        $ssh = $this->getService('ssh');
-
         $sshOptions = [];
-        $sshCommand = $ssh->getSshCommand($sshOptions);
+        $sshCommand = $this->ssh->getSshCommand($sshOptions);
         if ($this->isTerminal(STDIN)) {
             $sshCommand .= ' -t';
         }
         $sshCommand .= ' ' . escapeshellarg($sshUrl)
             . ' ' . escapeshellarg($redisCommand);
 
-        /** @var \Platformsh\Cli\Service\Shell $shell */
-        $shell = $this->getService('shell');
-
         $this->stdErr->writeln(
             sprintf('Connecting to Redis service via relationship <info>%s</info> on <info>%s</info>', $service['_relationship_name'], $sshUrl)
         );
 
-        return $shell->executeSimple($sshCommand);
+        return $this->shell->executeSimple($sshCommand);
     }
 }
