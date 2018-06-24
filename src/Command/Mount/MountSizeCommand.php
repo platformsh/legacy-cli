56a8a494cfebe1771e2fe5ce7715aa8cfea04921
Patrick Dawkins <pjcdawkins@gmail.com>
2018-06-24 15:31:59 +0100

    Refactor mount:size command; add --refresh option

--- src/Command/Mount/MountSizeCommand.php
+++ src/Command/Mount/MountSizeCommand.php
@@ -20,7 +20,8 @@ class MountSizeCommand extends MountCommandBase
         $this
             ->setName('mount:size')
             ->setDescription('Check the disk usage of mounts')
-            ->addOption('bytes', 'B', InputOption::VALUE_NONE, 'Show sizes in bytes');
+            ->addOption('bytes', 'B', InputOption::VALUE_NONE, 'Show sizes in bytes')
+            ->addOption('refresh', null, InputOption::VALUE_NONE, 'Refresh the cache');
         Table::configureInput($this->getDefinition());
         Ssh::configureInput($this->getDefinition());
         $this->addProjectOption();
@@ -37,7 +38,7 @@ class MountSizeCommand extends MountCommandBase
 
         $appName = $this->selectApp($input);
 
-        $appConfig = $this->getAppConfig($appName);
+        $appConfig = $this->getAppConfig($appName, $input->getOption('refresh'));
         if (empty($appConfig['mounts'])) {
             $this->stdErr->writeln(sprintf('The app "%s" doesn\'t define any mounts.', $appConfig['name']));
 
@@ -68,6 +69,7 @@ class MountSizeCommand extends MountCommandBase
         $commands[] = 'df -P -B1 -a -x squashfs -x tmpfs -x sysfs -x proc -x devpts -x rpc_pipefs';
         $commands[] = 'echo';
         $commands[] = 'cd "$' . $appDirVar . '"';
+
         foreach ($mountPaths as $mountPath) {
             $commands[] = 'du --block-size=1 -s ' . escapeshellarg($mountPath);
         }
@@ -88,7 +90,86 @@ class MountSizeCommand extends MountCommandBase
         // Separate the commands' output.
         list($appDir, $dfOutput, $duOutput) = explode("\n\n", $result, 3);
 
-        // Parse the output of 'df', building a list of results.
+        // Parse the output.
+        $volumeInfo = $this->parseDf($dfOutput, $appDir, $mountPaths);
+        $mountSizes = $this->parseDu($duOutput, $mountPaths);
+
+        // Build a table of results: one line per mount, one (multi-line) row
+        // per filesystem.
+        $header = ['Mount(s)', 'Size(s)', 'Disk', 'Used', 'Available', 'Capacity'];
+        $rows = [];
+        $showInBytes = $input->getOption('bytes');
+        foreach ($volumeInfo as $info) {
+            $row = [];
+            $row[] = implode("\n", $info['mounts']);
+            $mountUsage = [];
+            foreach ($info['mounts'] as $mountPath) {
+                $mountUsage[] = $mountSizes[$mountPath];
+            }
+            if ($showInBytes) {
+                $row[] = implode("\n", $mountUsage);
+                $row[] = $info['total'];
+                $row[] = $info['used'];
+                $row[] = $info['available'];
+            } else {
+                $row[] = implode("\n", array_map([Helper::class, 'formatMemory'], $mountUsage));
+                $row[] = Helper::formatMemory($info['total']);
+                $row[] = Helper::formatMemory($info['used']);
+                $row[] = Helper::formatMemory($info['available']);
+            }
+            $row[] = round($info['percent_used'], 1) . '%';
+            $rows[] = $row;
+        }
+
+
+        /** @var \Platformsh\Cli\Service\Table $table */
+        $table = $this->getService('table');
+        $table->render($rows, $header);
+
+        return 0;
+    }
+
+    /**
+     * Get a column from a line of df output.
+     *
+     * Unfortunately there doesn't seem to be a more reliable way to parse df
+     * output than by regular expression.
+     *
+     * @param string $line
+     * @param string $columnName
+     *
+     * @return string
+     */
+    private function getDfColumn($line, $columnName)
+    {
+        $columnPatterns = [
+            'filesystem' => '#^(.+?)(\s+[0-9])#',
+            'total' => '#([0-9]+)\s+[0-9]+\s+[0-9]+\s+[0-9]+%\s+#',
+            'used' => '#([0-9]+)\s+[0-9]+\s+[0-9]+%\s+#',
+            'available' => '#([0-9]+)\s+[0-9]+%\s+#',
+            'path' => '#%\s+(/.+)$#',
+        ];
+        if (!isset($columnPatterns[$columnName])) {
+            throw new \InvalidArgumentException("Invalid df column: $columnName");
+        }
+        if (!preg_match($columnPatterns[$columnName], $line, $matches)) {
+            throw new \RuntimeException("Failed to find column '$columnName' in df line: \n$line");
+        }
+
+        return trim($matches[1]);
+    }
+
+    /**
+     * Parse the output of 'df', building a list of results per FS volume.
+     *
+     * @param string $dfOutput
+     * @param string $appDir
+     * @param array  $mountPaths
+     *
+     * @return array
+     */
+    private function parseDf($dfOutput, $appDir, array $mountPaths)
+    {
         $results = [];
         foreach (explode("\n", $dfOutput) as $i => $line) {
             if ($i === 0) {
@@ -123,7 +204,19 @@ class MountSizeCommand extends MountCommandBase
             ];
         }
 
-        // Parse the 'du' output.
+        return $results;
+    }
+
+    /**
+     * Parse the 'du' output.
+     *
+     * @param string $duOutput
+     * @param array  $mountPaths
+     *
+     * @return array A list of mount sizes (in bytes) keyed by mount path.
+     */
+    private function parseDu($duOutput, array $mountPaths)
+    {
         $mountSizes = [];
         $duOutputSplit = explode("\n", $duOutput, count($mountPaths));
         foreach ($mountPaths as $i => $mountPath) {
@@ -133,68 +226,6 @@ class MountSizeCommand extends MountCommandBase
             list($mountSizes[$mountPath],) = explode("\t", $duOutputSplit[$i], 2);
         }
 
-        /** @var \Platformsh\Cli\Service\Table $table */
-        $table = $this->getService('table');
-
-        $header = ['Mount(s)', 'Size(s)', 'Disk', 'Used', 'Available', 'Capacity'];
-
-        $showInBytes = $input->getOption('bytes');
-
-        $rows = [];
-        foreach ($results as $info) {
-            $row = [];
-            $row[] = implode("\n", $info['mounts']);
-            $mountUsage = [];
-            foreach ($info['mounts'] as $mountPath) {
-                $mountUsage[] = $mountSizes[$mountPath];
-            }
-            if ($showInBytes) {
-                $row[] = implode("\n", $mountUsage);
-                $row[] = $info['total'];
-                $row[] = $info['used'];
-                $row[] = $info['available'];
-            } else {
-                $row[] = implode("\n", array_map([Helper::class, 'formatMemory'], $mountUsage));
-                $row[] = Helper::formatMemory($info['total']);
-                $row[] = Helper::formatMemory($info['used']);
-                $row[] = Helper::formatMemory($info['available']);
-            }
-            $row[] = round($info['percent_used'], 1) . '%';
-            $rows[] = $row;
-        }
-
-        $table->render($rows, $header);
-
-        return 0;
-    }
-
-    /**
-     * Get a column from a line of df output.
-     *
-     * Unfortunately there doesn't seem to be a more reliable way to parse df
-     * output than by regular expression.
-     *
-     * @param string $line
-     * @param string $columnName
-     *
-     * @return string
-     */
-    private function getDfColumn($line, $columnName)
-    {
-        $columnPatterns = [
-            'filesystem' => '#^(.+?)(\s+[0-9])#',
-            'total' => '#([0-9]+)\s+[0-9]+\s+[0-9]+\s+[0-9]+%\s+#',
-            'used' => '#([0-9]+)\s+[0-9]+\s+[0-9]+%\s+#',
-            'available' => '#([0-9]+)\s+[0-9]+%\s+#',
-            'path' => '#%\s+(/.+)$#',
-        ];
-        if (!isset($columnPatterns[$columnName])) {
-            throw new \InvalidArgumentException("Invalid df column: $columnName");
-        }
-        if (!preg_match($columnPatterns[$columnName], $line, $matches)) {
-            throw new \RuntimeException("Failed to find column '$columnName' in df line: \n$line");
-        }
-
-        return trim($matches[1]);
+        return $mountSizes;
     }
 }
