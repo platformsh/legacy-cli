65e79ea08900a7ebe7661621cbab97547b992c9b
Patrick Dawkins <pjcdawkins@users.noreply.github.com>
2018-07-16 12:38:13 +0100

    Add service:list (services) command (#731)
    * Add service:list (services) command
    
    * Refer apps and services commands to each other

--- src/Command/App/AppListCommand.php
+++ src/Command/App/AppListCommand.php
@@ -33,9 +33,23 @@ class AppListCommand extends CommandBase
         $this->validateInput($input);
 
         // Find a list of deployed web apps.
-        $apps = $this->api()
-            ->getCurrentDeployment($this->getSelectedEnvironment(), $input->getOption('refresh'))
-            ->webapps;
+        $deployment = $this->api()
+            ->getCurrentDeployment($this->getSelectedEnvironment(), $input->getOption('refresh'));
+        $apps = $deployment->webapps;
+
+        if (!count($apps)) {
+            $this->stdErr->writeln('No applications found.');
+
+            if ($deployment->services) {
+                $this->stdErr->writeln('');
+                $this->stdErr->writeln(sprintf(
+                    'To list services, run: <info>%s services</info>',
+                    $this->config()->get('application.executable')
+                ));
+            }
+
+            return 0;
+        }
 
         // Determine whether to show the "Local path" of the app. This will be
         // found via matching the remote, deployed app with one in the local
@@ -75,6 +89,24 @@ class AppListCommand extends CommandBase
 
         /** @var \Platformsh\Cli\Service\Table $table */
         $table = $this->getService('table');
+        if (!$table->formatIsMachineReadable()) {
+            $this->stdErr->writeln(sprintf(
+                'Applications on the project <info>%s</info>, environment <info>%s</info>:',
+                $this->api()->getProjectLabel($this->getSelectedProject()),
+                $this->api()->getEnvironmentLabel($this->getSelectedEnvironment())
+            ));
+        }
+
         $table->render($rows, $headers);
+
+        if (!$table->formatIsMachineReadable() && $deployment->services) {
+            $this->stdErr->writeln('');
+            $this->stdErr->writeln(sprintf(
+                'To list services, run: <info>%s services</info>',
+                $this->config()->get('application.executable')
+            ));
+        }
+
+        return 0;
     }
 }
