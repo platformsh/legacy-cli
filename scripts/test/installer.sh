0e665c8f282331db7ba6ae29bad92ad163e70de8
Patrick Dawkins <pjcdawkins@gmail.com>
2018-09-10 09:45:54 +0100

    Fix installer test script to use correct manifest

--- scripts/test/installer.sh
+++ scripts/test/installer.sh
@@ -2,4 +2,5 @@
 # Tests the CLI installer.
 # This must be run from the repository root.
 
-PLATFORMSH_CLI_MANIFEST_URL=./dist/manifest.json cat ./dist/installer.php | php
+export PLATFORMSH_CLI_MANIFEST_URL=./dist/manifest.json
+cat ./dist/installer.php | php
