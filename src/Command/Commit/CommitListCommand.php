5d4151835bb94ca974e9bc0d6e2721200f57be8e
Patrick Dawkins <pjcdawkins@gmail.com>
2018-11-05 18:42:06 +0000

    Avoid wrapping dates in commit:list

--- src/Command/Commit/CommitListCommand.php
+++ src/Command/Commit/CommitListCommand.php
@@ -79,7 +79,10 @@ class CommitListCommand extends CommandBase
         $rows = [];
         foreach ($commits as $commit) {
             $row = [];
-            $row[] = $formatter->format($commit->author['date'], 'author.date');
+            $row[] = new AdaptiveTableCell(
+                $formatter->format($commit->author['date'], 'author.date'),
+                ['wrap' => false]
+            );
             $row[] = new AdaptiveTableCell($commit->sha, ['wrap' => false]);
             $row[] = $commit->author['name'];
             $row[] = $this->summarize($commit->message);
