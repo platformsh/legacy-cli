e369565b1961c3afab8f16ead303c4ce225122f9
Patrick Dawkins <pjcdawkins@gmail.com>
2019-06-19 16:44:02 +0100

    Support reading routes from the local environment in route:get command

--- src/Command/Route/RouteGetCommand.php
+++ src/Command/Route/RouteGetCommand.php
@@ -3,6 +3,7 @@ namespace Platformsh\Cli\Command\Route;
 
 use Platformsh\Cli\Command\CommandBase;
 use Platformsh\Cli\Service\PropertyFormatter;
+use Platformsh\Client\Model\Route;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -31,23 +32,62 @@ class RouteGetCommand extends CommandBase
         $this->addExample('View the URL to the https://{default}/ route', "'https://{default}/' -P url");
     }
 
+    /**
+     * @return bool
+     */
+    private function isLocalOnContainer(InputInterface $input) {
+        $envPrefix = $this->config()->get('service.env_prefix');
+        if ($input->getOption('project')
+            && getenv($envPrefix . 'PROJECT')
+            && getenv($envPrefix . 'PROJECT') !== $input->getOption('project')) {
+            return false;
+        }
+        if ($input->getOption('environment')
+            && getenv($envPrefix . 'BRANCH')
+            && getenv($envPrefix . 'BRANCH') !== $input->getOption('environment')) {
+            return false;
+        }
+        if ($input->getOption('app')
+            && getenv($envPrefix . 'APPLICATION_NAME')
+            && getenv($envPrefix . 'APPLICATION_NAME') !== $input->getOption('app')) {
+            return false;
+        }
+
+        return true;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateInput($input);
+        // Allow override via PLATFORM_ROUTES.
+        $prefix = $this->config()->get('service.env_prefix');
+        if (getenv($prefix . 'ROUTES') && $this->isLocalOnContainer($input)) {
+            $decoded = json_decode(base64_decode(getenv($prefix . 'ROUTES'), true), true);
+            if (empty($decoded)) {
+                throw new \RuntimeException('Failed to decode: ' . $prefix . 'ROUTES');
+            }
+            $routes = $decoded;
+        } else {
+            $this->validateInput($input);
+            $environment = $this->getSelectedEnvironment();
+            $deployment = $this->api()
+                ->getCurrentDeployment($environment, $input->getOption('refresh'));
+            $routes = array_map(function (Route $route) {
+                return $route->getProperties();
+            }, $deployment->routes);
+        }
+
         $this->warnAboutDeprecatedOptions(['app', 'identity-file']);
 
-        $environment = $this->getSelectedEnvironment();
-
-        $routes = $this->api()->getCurrentDeployment($environment, $input->getOption('refresh'))->routes;
-
+        /** @var array|false $selectedRoute */
         $selectedRoute = false;
+
         $id = $input->getOption('id');
         if (!$selectedRoute && $id !== null) {
             foreach ($routes as $url => $route) {
-                $properties = $route->getProperties();
-                if (isset($properties['id']) && $properties['id'] === $id) {
-                    $selectedRoute = $properties;
+                if (isset($route['id']) && $route['id'] === $id) {
+                    $selectedRoute = $route;
                     $selectedRoute['url'] = $url;
+                    break;
                 }
             }
             if (!$selectedRoute) {
@@ -59,10 +99,10 @@ class RouteGetCommand extends CommandBase
 
         if (!$selectedRoute && $input->getOption('primary')) {
             foreach ($routes as $url => $route) {
-                $properties = $route->getProperties();
-                if (isset($properties['primary']) && $properties['primary']) {
-                    $selectedRoute = $properties;
+                if (!empty($route['primary'])) {
+                    $selectedRoute = $route;
                     $selectedRoute['url'] = $url;
+                    break;
                 }
             }
             if (!$selectedRoute) {
@@ -83,10 +123,10 @@ class RouteGetCommand extends CommandBase
             foreach ($routes as $route) {
                 $originalUrl = $route->original_url;
                 $items[$originalUrl] = $originalUrl;
-                if (!empty($route->id)) {
-                    $items[$originalUrl] .= ' (<info>' . $route->id . '</info>)';
+                if (!empty($route['id'])) {
+                    $items[$originalUrl] .= ' (<info>' . $route['id'] . '</info>)';
                 }
-                if (!empty($route->primary)) {
+                if (!empty($route['primary'])) {
                     $items[$originalUrl] .= ' - <info>primary</info>';
                 }
             }
@@ -96,8 +136,8 @@ class RouteGetCommand extends CommandBase
 
         if (!$selectedRoute && $originalUrl !== null) {
             foreach ($routes as $url => $route) {
-                if ($route->original_url === $originalUrl) {
-                    $selectedRoute = $route->getProperties();
+                if (isset($route['original_url']) && $route['original_url'] === $originalUrl) {
+                    $selectedRoute = $route;
                     $selectedRoute['url'] = $url;
                     break;
                 }
@@ -115,6 +155,9 @@ class RouteGetCommand extends CommandBase
 
             return 1;
         }
+
+        // Add defaults.
+        $selectedRoute += ['primary' => false, 'id' => null];
 
         /** @var PropertyFormatter $propertyFormatter */
         $propertyFormatter = $this->getService('property_formatter');
