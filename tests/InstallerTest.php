d4e451ae21b835d2790eb0dc385b1b503e210386
Patrick Dawkins <pjcdawkins@gmail.com>
2019-06-27 11:18:36 +0100

    Installer: version constraints via --min and --max

--- tests/InstallerTest.php
+++ tests/InstallerTest.php
@@ -2,6 +2,7 @@
 
 namespace Platformsh\Cli\Tests;
 
+use Platformsh\Cli\Installer\Installer;
 use Platformsh\Cli\Installer\VersionResolver;
 
 class InstallerTest extends \PHPUnit_Framework_TestCase
@@ -50,6 +51,47 @@ class InstallerTest extends \PHPUnit_Framework_TestCase
                 'php' => ['min' => '5.5.9'],
             ]
         ], '5.5.0'));
+    }
+
+    public function testFindLatestVersionWithMax()
+    {
+        $this->assertEquals('3.0.0', (new VersionResolver())->findLatestVersion([
+            ['version' => '1.0.0'],
+            ['version' => '2.0.0'],
+            ['version' => '3.0.0'],
+            ['version' => '3.0.1'],
+        ], '', '3.0.0')['version']);
+    }
+
+    public function testFindLatestVersionWithMin()
+    {
+        $this->assertEquals('3.0.1', (new VersionResolver())->findLatestVersion([
+            ['version' => '1.0.0'],
+            ['version' => '2.0.0'],
+            ['version' => '3.0.0'],
+            ['version' => '3.0.1'],
+        ], '2.0')['version']);
+
+        $this->setExpectedException(\RuntimeException::class);
+        (new VersionResolver())->findLatestVersion([
+            ['version' => '1.0.0'],
+            ['version' => '2.0.0'],
+            ['version' => '3.0.0'],
+            ['version' => '3.0.1'],
+        ], 'v3.1');
+    }
+
+    public function testGetOption()
+    {
+        $method = new \ReflectionMethod(Installer::class, 'getOption');
+        $method->setAccessible(true);
+        $args = ['--min', '1.2.3'];
+        $this->assertEquals('1.2.3', $method->invoke(new Installer($args), 'min'));
+        $args = ['--max', '2.3.4'];
+        $this->assertEquals('', $method->invoke(new Installer($args), 'min'));
+        $this->assertEquals('2.3.4', $method->invoke(new Installer($args), 'max'));
+        $args = ['--max=2.0.0'];
+        $this->assertEquals('2.0.0', $method->invoke(new Installer($args), 'max'));
     }
 
 }
