331db4e5e533d8d8f9e9d9db658248b0d5385c47
Patrick Dawkins <pjcdawkins@gmail.com>
2019-07-02 23:14:19 +0100

    Adapt welcome command for offline-on-container uses

---
+++ tests/Command/WelcomeCommandTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Platformsh\Cli\Tests\Command\App;
+
+use Platformsh\Cli\Command\WelcomeCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class WelcomeCommandTest extends \PHPUnit_Framework_TestCase
+{
+    private function runCommand(array $args) {
+        $output = new BufferedOutput();
+        $input = new ArrayInput($args);
+        $input->setInteractive(false);
+        (new WelcomeCommand())
+            ->run($input, $output);
+
+        return $output->fetch();
+    }
+
+    public function testWelcomeOnLocalContainer() {
+        chdir('/');
+        putenv('PLATFORM_PROJECT=test-project');
+        putenv('PLATFORM_BRANCH=test-environment');
+        putenv('PLATFORM_ROUTES=' . base64_encode(json_encode([])));
+        putenv('PLATFORMSH_CLI_SESSION_ID=test' . rand(100, 999));
+        $result = $this->runCommand([]);
+        $this->assertContains(
+            'Project ID: test-project',
+            $result
+        );
+        $this->assertContains(
+            'Local environment commands',
+            $result
+        );
+    }
+}
