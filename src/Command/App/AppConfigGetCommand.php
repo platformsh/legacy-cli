303969b3529c16b0a0ebd1f0a682504bd4ec9807
Patrick Dawkins <pjcdawkins@gmail.com>
2019-07-03 00:28:43 +0100

    Allow app:config-get to read from PLATFORM_APPLICATION

--- src/Command/App/AppConfigGetCommand.php
+++ src/Command/App/AppConfigGetCommand.php
@@ -2,6 +2,7 @@
 namespace Platformsh\Cli\Command\App;
 
 use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Model\AppConfig;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -29,15 +30,25 @@ class AppConfigGetCommand extends CommandBase
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateInput($input);
-        $this->warnAboutDeprecatedOptions(['identity-file']);
+        // Allow override via PLATFORM_APPLICATION.
+        $prefix = $this->config()->get('service.env_prefix');
+        if (getenv($prefix . 'APPLICATION') && !$this->doesEnvironmentConflictWithCommandLine($input)) {
+            $this->debug('Reading application config from environment variable ' . $prefix . 'APPLICATION');
+            $decoded = json_decode(base64_decode(getenv($prefix . 'APPLICATION'), true), true);
+            if (!is_array($decoded)) {
+                throw new \RuntimeException('Failed to decode: ' . $prefix . 'APPLICATION');
+            }
+            $appConfig = new AppConfig($decoded);
+        } else {
+            $this->validateInput($input);
+            $this->warnAboutDeprecatedOptions(['identity-file']);
 
-        $appConfig = $this->selectRemoteContainer($input, false)
-            ->getConfig()
-            ->getNormalized();
+            $appConfig = $this->selectRemoteContainer($input, false)
+                ->getConfig();
+        }
 
         /** @var \Platformsh\Cli\Service\PropertyFormatter $formatter */
         $formatter = $this->getService('property_formatter');
-        $formatter->displayData($output, $appConfig, $input->getOption('property'));
+        $formatter->displayData($output, $appConfig->getNormalized(), $input->getOption('property'));
     }
 }
