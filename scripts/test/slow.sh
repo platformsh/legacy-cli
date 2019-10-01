d97748188311cd52e943676d7557ccbf4b618c18
Patrick Dawkins <pjcdawkins@users.noreply.github.com>
2019-10-01 14:25:54 +0100

    Make unit tests verbose

--- scripts/test/slow.sh
+++ scripts/test/slow.sh
@@ -2,4 +2,4 @@
 # Runs slow PhpUnit tests.
 # This must be run from the repository root.
 
-./vendor/bin/phpunit -c ./phpunit.xml --group slow
+./vendor/bin/phpunit -c ./phpunit.xml --group slow --verbose
--- scripts/test/unit.sh
+++ scripts/test/unit.sh
@@ -2,4 +2,4 @@
 # Runs PhpUnit tests.
 # This must be run from the repository root.
 
-./vendor/bin/phpunit -c ./phpunit.xml --coverage-text --exclude-group slow
+./vendor/bin/phpunit -c ./phpunit.xml --coverage-text --exclude-group slow --verbose
