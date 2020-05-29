c1bba4c37dd298ada8150792407dfe1e3890ff2e
Patrick Dawkins <patrick@platform.sh>
2020-05-29 12:09:12 +0100

    Separate dependencies tests

--- tests/Local/BuildFlavor/DependenciesTest.php
+++ tests/Local/BuildFlavor/DependenciesTest.php
@@ -2,6 +2,8 @@
 
 namespace Platformsh\Cli\Tests\BuildFlavor;
 
+use Platformsh\Cli\Service\Shell;
+
 /**
  * @group slow
  */
@@ -19,8 +21,43 @@ class DependenciesTest extends BaseBuildFlavorTest
         $this->assertBuildSucceeds($this->sourceDir, ['no-deps' => true], false);
     }
 
-    public function testBuildSucceedsIfDepsInstalled()
+    public function testBuildSucceedsIfNodejsDepsInstalled()
     {
-        $this->assertBuildSucceeds($this->sourceDir);
+        $shell = new Shell();
+        if ($shell->commandExists('npm')) {
+            $this->assertBuildSucceeds($this->sourceDir . '/nodejs');
+        } else {
+            $this->markTestSkipped();
+        }
+    }
+
+    public function testBuildSucceedsIfPhpDepsInstalled()
+    {
+        $shell = new Shell();
+        if ($shell->commandExists('composer')) {
+            $this->assertBuildSucceeds($this->sourceDir . '/php');
+        } else {
+            $this->markTestSkipped();
+        }
+    }
+
+    public function testBuildSucceedsIfPythonDepsInstalled()
+    {
+        $shell = new Shell();
+        if ($shell->commandExists('pip') || $shell->commandExists('pip3')) {
+            $this->assertBuildSucceeds($this->sourceDir . '/python');
+        } else {
+            $this->markTestSkipped();
+        }
+    }
+
+    public function testBuildSucceedsIfRubyDepsInstalled()
+    {
+        $shell = new Shell();
+        if ($shell->commandExists('bundle')) {
+            $this->assertBuildSucceeds($this->sourceDir . '/ruby');
+        } else {
+            $this->markTestSkipped();
+        }
     }
 }
