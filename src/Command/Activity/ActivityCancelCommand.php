e4b01b37be02ee75587d3cf1451176e37cdf0845
Patrick Dawkins <patrick@platform.sh>
2020-12-10 20:21:53 -0500

    Add activity choice

--- src/Command/Activity/ActivityCancelCommand.php
+++ src/Command/Activity/ActivityCancelCommand.php
@@ -5,6 +5,8 @@ use GuzzleHttp\Exception\BadResponseException;
 use Platformsh\Cli\Command\CommandBase;
 use Platformsh\Cli\Service\ActivityLoader;
 use Platformsh\Cli\Service\ActivityMonitor;
+use Platformsh\Cli\Service\PropertyFormatter;
+use Platformsh\Cli\Service\QuestionHelper;
 use Platformsh\Client\Model\Activity;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -56,13 +58,14 @@ class ActivityCancelCommand extends CommandBase
                 }
             }
         } else {
-            $activities = $loader->loadFromInput($apiResource, $input, 1, [Activity::STATE_PENDING, Activity::STATE_IN_PROGRESS], 'cancel');
+            $activities = $loader->loadFromInput($apiResource, $input, 10, [Activity::STATE_PENDING, Activity::STATE_IN_PROGRESS], 'cancel');
             if ($activities === false) {
                 return 1;
             }
-            /** @var Activity $activity */
-            $activity = reset($activities);
-            if (!$activity) {
+            $activities = \array_filter($activities, function (Activity $activity) {
+                return $activity->operationAvailable('cancel');
+            });
+            if (\count($activities) === 0) {
                 $this->stdErr->writeln('No cancellable activities found');
 
                 $this->stdErr->writeln('');
@@ -70,6 +73,24 @@ class ActivityCancelCommand extends CommandBase
 
                 return 1;
             }
+            $choices = [];
+            /** @var QuestionHelper $questionHelper */
+            $questionHelper = $this->getService('question_helper');
+            /** @var PropertyFormatter $formatter */
+            $formatter = $this->getService('property_formatter');
+            $byId = [];
+            $this->api()->sortResources($activities, 'created_at');
+            foreach ($activities as $activity) {
+                $byId[$activity->id] = $activity;
+                $choices[$activity->id] = \sprintf(
+                    '%s: %s (%s)',
+                    $formatter->formatDate($activity->created_at),
+                    ActivityMonitor::getFormattedDescription($activity),
+                    ActivityMonitor::formatState($activity->state)
+                );
+            }
+            $id = $questionHelper->choose($choices, 'Enter a number to choose an activity to cancel:', key($choices), true);
+            $activity = $byId[$id];
         }
 
         $this->stdErr->writeln(sprintf(
@@ -82,6 +103,7 @@ class ActivityCancelCommand extends CommandBase
             $activity->cancel();
         } catch (BadResponseException $e) {
             if ($e->getResponse() && $e->getResponse()->getStatusCode() === 400 && \strpos($e->getMessage(), 'cannot be cancelled in its current state')) {
+                $activity->refresh();
                 $this->stdErr->writeln(\sprintf('The activity cannot be cancelled in its current state (<error>%s</error>).', $activity->state));
                 $this->stdErr->writeln('');
                 $this->stdErr->writeln(\sprintf("To view this activity's log, run: <comment>%s activity:log %s</comment>", $executable, $activity->id));
