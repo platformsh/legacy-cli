428e1d070cfaaacd8efb6c7713e6ecafe2fc8dbe
Patrick Dawkins <patrick@platform.sh>
2020-12-12 09:25:38 -0500

    Disable Travis coverage to fix unit tests on PHP 7.3

--- scripts/test/unit.sh
+++ scripts/test/unit.sh
@@ -2,4 +2,4 @@
 # Runs PhpUnit tests.
 # This must be run from the repository root.
 
-./vendor/bin/phpunit -c ./phpunit.xml --coverage-text --exclude-group slow --verbose
+./vendor/bin/phpunit -c ./phpunit.xml --exclude-group slow --verbose
