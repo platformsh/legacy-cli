3e1cc8e329f985e18d108fa1d086dd55c44dc481
Patrick Dawkins <patrick@platform.sh>
2020-12-16 15:31:43 -0500

    Unused import

--- src/Command/CommandBase.php
+++ src/Command/CommandBase.php
@@ -26,7 +26,6 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
-use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Terminal;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
