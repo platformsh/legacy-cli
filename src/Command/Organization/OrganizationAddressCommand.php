cfa7fb7d2e156c24c70fb31b4cc6c49d862da6ce
Patrick Dawkins <patrick@platform.sh>
2021-08-18 13:41:37 +0100

    Add organization address and profile commands

--- src/Command/Organization/OrganizationAddressCommand.php
+++ src/Command/Organization/OrganizationAddressCommand.php
@@ -5,86 +5,76 @@ use GuzzleHttp\Exception\BadResponseException;
 use Platformsh\Cli\Console\AdaptiveTableCell;
 use Platformsh\Cli\Service\PropertyFormatter;
 use Platformsh\Cli\Service\Table;
-use Platformsh\Client\Model\Organization\Organization;
+use Platformsh\Client\Model\Organization\Address;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class OrganizationInfoCommand extends OrganizationCommandBase
+class OrganizationAddressCommand extends OrganizationCommandBase
 {
 
     protected function configure()
     {
-        $this->setName('organization:info')
-            ->setDescription('View or change organization details')
+        $this->setName('organization:address')
+            ->setDescription("View or change an organization's billing address")
             ->addOrganizationOptions()
             ->addArgument('property', InputArgument::OPTIONAL, 'The name of a property to view or change')
             ->addArgument('value', InputArgument::OPTIONAL, 'A new value for the property');
         PropertyFormatter::configureInput($this->getDefinition());
         Table::configureInput($this->getDefinition());
-        $this->addExample('View the organization "acme"', '--org acme')
-            ->addExample("Show the organization's label", '--org acme label')
-            ->addExample('Change the organization label', '--org acme label "ACME Inc."');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $organization = $this->validateOrganizationInput($input);
+        $org = $this->validateOrganizationInput($input);
+        $address = $org->getAddress();
 
         /** @var PropertyFormatter $formatter */
         $formatter = $this->getService('property_formatter');
 
         $property = $input->getArgument('property');
         if ($property === null) {
-            $this->listProperties($organization);
+            $headings = [];
+            $values = [];
+            /** @var PropertyFormatter $formatter */
+            $formatter = $this->getService('property_formatter');
+            foreach ($address->getProperties() as $key => $value) {
+                $headings[] = new AdaptiveTableCell($key, ['wrap' => false]);
+                $values[] = $formatter->format($value, $key);
+            }
+
+            /** @var Table $table */
+            $table = $this->getService('table');
+
+            if (!$table->formatIsMachineReadable()) {
+                $this->stdErr->writeln(\sprintf('Billing address for the organization %s:', $this->api()->getOrganizationLabel($org)));
+            }
+
+            $table->renderSimple($values, $headings);
+
+            if (!$table->formatIsMachineReadable()) {
+                $this->stdErr->writeln(\sprintf('To view organization details, run: <info>%s</info>', $this->otherCommandExample($input, 'org:info')));
+            }
             return 0;
         }
 
         $value = $input->getArgument('value');
         if ($value === null) {
-            $formatter->displayData($output, $this->getProperties($organization), $property);
+            $formatter->displayData($output, $address->getProperties(), $property);
             return 0;
         }
 
-        return $this->setProperty($property, $value, $organization);
-    }
-
-    private function getProperties(Organization $organization)
-    {
-        $data = $organization->getProperties();
-
-        // Set the owner from the user reference.
-        if (isset($data['owner_id']) && isset($data['ref:users'][$data['owner_id']])) {
-            $data['owner'] = $data['ref:users'][$data['owner_id']]->getProperties();
-            unset($data['ref:users']);
-        }
-
-        return $data;
-    }
-
-    private function listProperties(Organization $organization)
-    {
-        $headings = [];
-        $values = [];
-        /** @var PropertyFormatter $formatter */
-        $formatter = $this->getService('property_formatter');
-        foreach ($this->getProperties($organization) as $key => $value) {
-            $headings[] = new AdaptiveTableCell($key, ['wrap' => false]);
-            $values[] = $formatter->format($value, $key);
-        }
-        /** @var Table $table */
-        $table = $this->getService('table');
-        $table->renderSimple($values, $headings);
+        return $this->setProperty($property, $value, $address);
     }
 
     /**
-     * @param string      $property
-     * @param string      $value
-     * @param Organization $organization
+     * @param string  $property
+     * @param string  $value
+     * @param Address $address
      *
      * @return int
      */
-    protected function setProperty($property, $value, Organization $organization)
+    protected function setProperty($property, $value, Address $address)
     {
         if (!$this->validateValue($property, $value)) {
             return 1;
@@ -92,18 +82,18 @@ class OrganizationInfoCommand extends OrganizationCommandBase
         /** @var PropertyFormatter $formatter */
         $formatter = $this->getService('property_formatter');
 
-        $currentValue = $organization->getProperty($property, false);
+        $currentValue = $address->getProperty($property, false);
         if ($currentValue === $value) {
             $this->stdErr->writeln(sprintf(
                 'Property <info>%s</info> already set as: %s',
                 $property,
-                $formatter->format($organization->getProperty($property, false), $property)
+                $formatter->format($address->getProperty($property, false), $property)
             ));
 
             return 0;
         }
         try {
-            $organization->update([$property => $value]);
+            $address->update([$property => $value]);
         } catch (BadResponseException $e) {
             // Translate validation error messages.
             if (($response = $e->getResponse()) && $response->getStatusCode() === 400 && ($body = $response->getBody())) {
@@ -122,7 +112,7 @@ class OrganizationInfoCommand extends OrganizationCommandBase
         $this->stdErr->writeln(sprintf(
             'Property <info>%s</info> set to: %s',
             $property,
-            $formatter->format($organization->$property, $property)
+            $formatter->format($address->$property, $property)
         ));
 
         return 0;
@@ -138,8 +128,16 @@ class OrganizationInfoCommand extends OrganizationCommandBase
     private function getType($property)
     {
         $writableProperties = [
-            'name' => 'string',
-            'label' => 'string',
+            'country' => 'string',
+            'name_line' => 'string',
+            'premise' => 'string',
+            'sub_premise' => 'string',
+            'thoroughfare' => 'string',
+            'administrative_area' => 'string',
+            'sub_administrative_area' => 'string',
+            'locality' => 'string',
+            'dependent_locality' => 'string',
+            'postal_code' => 'string',
         ];
 
         return isset($writableProperties[$property]) ? $writableProperties[$property] : false;
