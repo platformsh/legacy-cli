fd61c99597000e9494b1b26ba598ee2e94b8a395
Patrick Dawkins <patrick@platform.sh>
2021-09-08 08:17:18 +0100

    Include organization:list in the commands that are disabled by config

--- src/Command/Organization/OrganizationListCommand.php
+++ src/Command/Organization/OrganizationListCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class OrganizationListCommand extends CommandBase
+class OrganizationListCommand extends OrganizationCommandBase
 {
 
     /**
