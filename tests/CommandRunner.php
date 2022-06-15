f9115357252d6d96c290a1dc5e1511ea752972ec
Patrick Dawkins <patrick@platform.sh>
2022-06-15 17:16:45 +0100

    Fix tests not setting local environment variables correctly

--- tests/CommandRunner.php
+++ tests/CommandRunner.php
@@ -2,6 +2,8 @@
 
 namespace Platformsh\Cli\Tests;
 
+use Symfony\Component\Process\Process;
+
 class CommandRunner
 {
     /**
@@ -13,31 +15,22 @@ class CommandRunner
      * @return \Platformsh\Cli\Tests\CommandResult
      */
     public function run(string $commandName, array $args = [], array $env = [], bool $allowFailure = false): CommandResult {
-        $descriptorSpec = [
-            0 => ['pipe', 'r'],
-            1 => ['pipe', 'w'],
-            2 => ['pipe', 'w'],
-        ];
         $path = realpath(__DIR__ . '/../bin/platform');
         if (!$path) {
             throw new \RuntimeException('Cannot find executable');
         }
         $args = array_merge([$path, $commandName], $args);
-        $cmd = implode(' ', array_map('escapeshellarg', $args));
-        $process = proc_open($cmd, $descriptorSpec, $pipes, null, $env + getenv());
-        $output = stream_get_contents($pipes[1]);
-        fclose($pipes[1]);
-        $errorOutput = stream_get_contents($pipes[2]);
-        fclose($pipes[2]);
-        $exitCode = proc_close($process);
+        $process = new Process($args, null, $env + \getenv());
+        $process->run();
+        $exitCode = $process->getExitCode();
         if (!$allowFailure && $exitCode !== 0) {
             throw new \RuntimeException(
-                "Command failed with exit code $exitCode: $cmd"
-                . "\nOutput: $output"
-                . "\nError output: $errorOutput"
+                "Command failed with exit code $exitCode: {$process->getCommandLine()}"
+                . "\nOutput: {$process->getOutput()}"
+                . "\nError output: {$process->getErrorOutput()}"
             );
         }
 
-        return new CommandResult($exitCode, $output, $errorOutput);
+        return new CommandResult($exitCode, $process->getOutput(), $process->getErrorOutput());
     }
 }
