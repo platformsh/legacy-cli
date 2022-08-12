2f3a979e4012c954db54beff2abbc77b1f13aa96
Tyler Mills <tylermills.v1@gmail.com>
2022-08-12 15:50:59 +0000

    Gitpod uses bash by default

--- .gitpod/scripts/alias-dev-cli.sh
+++ .gitpod/scripts/alias-dev-cli.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "alias platfarm=\$GITPOD_REPO_ROOT/bin/platform" >> ~/.zshrc
-echo "alias pf=platfarm" >> ~/.zshrc
+echo "alias platfarm=\$GITPOD_REPO_ROOT/bin/platform" >> ~/.bashrc
+echo "alias pf=platfarm" >> ~/.bashrc
 
-source ~/.zshrc
+source ~/.bashrc
 
 echo -e "\e[32mYou can run Platform CLI commands using: platfarm or pf\e[0m"
 echo -e "\e[1;33mNote: That's \"platfarm\" not \"platform\". The \"platform\" command needs to be available when you test builds and installs.\e[0m"
