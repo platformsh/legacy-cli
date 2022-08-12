d1376ca1c066597d695bc9cd429c70d46128cf60
Tyler Mills <tylermills.v1@gmail.com>
2022-08-12 17:07:13 +0000

    Clean up login.sh for gitpod

--- .gitpod/scripts/login.sh
+++ .gitpod/scripts/login.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [ -z "$PLATFORM_CLI_TOKEN" ]; then
-    echo "\$PLATFORMSH_CLI_TOKEN was detected as an environment variable which.";
-    echo "You may updated it with \`eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)\`.";
+    echo "\$PLATFORMSH_CLI_TOKEN was detected as an environment variable. The Platform CLI will authenticate using that token.";
+    echo "You may updated it with: eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)";
 else
     echo "You have not set your Platform API token yet. Paste it now or press <return> to continue without one.";
     read NEW_TOKEN;
     if [ -z "$NEW_TOKEN" ]; then
-        echo "Okay, you can always set it later \`eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)\`.";
+        echo "Okay, you can always set it later: eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)";
     else
         echo "Logging into Platform.sh using ${NEW_TOKEN}.";
         eval $(gp env -e PLATFORMSH_CLI_TOKEN=$NEW_TOKEN);
-        $GITPOD_REPO_ROOT/bin/platform auth:info || echo "Could not log in using ${NEW_TOKEN}. Please provide a new one using: \`eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)\`.";
+        $GITPOD_REPO_ROOT/bin/platform auth:info || echo "Could not log in using ${NEW_TOKEN}. Please provide a new one using: eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)";
     fi;
 fi;
