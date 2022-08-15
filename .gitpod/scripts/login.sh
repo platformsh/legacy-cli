bb7a45059b56c269021856eb7c88ca4607174516
Tyler Mills <tylerssn@users.noreply.github.com>
2022-08-15 10:35:35 -0400

    Suggest authentication steps during setup
    * Provide authentication hints
    * Increase security: Gitpod environments are shared, therefore their environment variables are shared.

--- .gitpod/scripts/login.sh
+++ .gitpod/scripts/login.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
+echo "When ready to make authenticated requests, use: platform auth:api-token-login"
+echo "Or: export \PLATFORMSH_CLI_TOKEN=YOUR_TOKEN"
+
 if [ -z "$PLATFORM_CLI_TOKEN" ]; then
-    echo "\$PLATFORMSH_CLI_TOKEN was detected as an environment variable. The Platform CLI will authenticate using that token.";
-    echo "You may updated it with: eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)";
-else
-    echo "You have not set your Platform API token yet. Paste it now or press <return> to continue without one.";
-    read NEW_TOKEN;
-    if [ -z "$NEW_TOKEN" ]; then
-        echo "Okay, you can always set it later: eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)";
-    else
-        echo "Logging into Platform.sh using ${NEW_TOKEN}.";
-        eval $(gp env -e PLATFORMSH_CLI_TOKEN=$NEW_TOKEN);
-        $GITPOD_REPO_ROOT/bin/platform auth:info || echo "Could not log in using ${NEW_TOKEN}. Please provide a new one using: eval $(gp env -e PLATFORMSH_CLI_TOKEN=NEW_TOKEN)";
-    fi;
+    echo "WARNING: \$PLATFORMSH_CLI_TOKEN was detected as an environment variable. If you share this workspace with others, they will have access to this token.";
 fi;
