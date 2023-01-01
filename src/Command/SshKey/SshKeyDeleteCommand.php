5ff5b1a426fabe738193f68c9cdc7e2209a2b77e
Patrick Dawkins <patrick@platform.sh>
2023-01-01 20:55:54 +0000

    Support interactive selection in ssh-key:delete

--- src/Command/SshKey/SshKeyDeleteCommand.php
+++ src/Command/SshKey/SshKeyDeleteCommand.php
@@ -27,10 +27,25 @@ class SshKeyDeleteCommand extends SshKeyCommandBase
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $id = $input->getArgument('id');
+        if (empty($id) && $input->isInteractive()) {
+            $keys = $this->api()->getSshKeys(true);
+            if (empty($keys)) {
+                $this->stdErr->writeln('You do not have any SSH keys in your account.');
+                return 1;
+            }
+            $options = [];
+            foreach ($keys as $key) {
+                $options[$key->key_id] = sprintf('%s (%s)', $key->key_id, $key->title ?: $key->fingerprint);
+            }
+            /** @var \Platformsh\Cli\Service\QuestionHelper $questionHelper */
+            $questionHelper = $this->getService('question_helper');
+            $id = $questionHelper->choose($options, 'Enter a number to choose a key to delete:', null, false);
+        }
         if (empty($id) || !is_numeric($id)) {
             $this->stdErr->writeln('<error>You must specify the ID of the SSH key to delete.</error>');
+            $this->stdErr->writeln('');
             $this->stdErr->writeln(
-                'List your SSH keys with: <info>' . $this->config()->get('application.executable') . ' ssh-keys</info>'
+                'List your SSH keys with: <comment>' . $this->config()->get('application.executable') . ' ssh-keys</comment>'
             );
 
             return 1;
