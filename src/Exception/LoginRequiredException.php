42dcd2048de4118213618d3caec65ce5bbc5590a
Patrick Dawkins <patrick@platform.sh>
2023-05-31 15:46:20 +0100

    Improve login required message

--- src/Exception/LoginRequiredException.php
+++ src/Exception/LoginRequiredException.php
@@ -18,8 +18,10 @@ class LoginRequiredException extends HttpException
         $message = $message ?: $this->message;
         $this->config = $config ?: new Config();
         $executable = $this->config->get('application.executable');
+        $envPrefix = $this->config->get('application.env_prefix');
         $message .= "\n\nPlease log in by running:\n    <comment>$executable login</comment>"
-            . "\n\nTo log in using an API token, run: <comment>$executable auth:api-token-login</comment>";
+            . "\n\nAlternatively, to log in using an API token (without a browser), run: <comment>$executable auth:api-token-login</comment>"
+            . "\n\nTo authenticate non-interactively, configure an API token using the <comment>{$envPrefix}TOKEN</comment> environment variable.";
 
         parent::__construct($message, $previous);
     }
