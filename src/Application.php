1cb0f6647b367877fba07a9fd23cd7d7d231658f
Patrick Dawkins <pjcdawkins@users.noreply.github.com>
2023-06-14 15:57:33 +0100

    Add env:pause and env:resume commands (#1268)
    Development environments are automatically paused after two weeks of inactivity
    to save resources and reduce carbon emissions.
    
    These commands add some manual control.

--- src/Application.php
+++ src/Application.php
@@ -155,9 +155,11 @@ class Application extends ParentApplication
         $commands[] = new Command\Environment\EnvironmentInfoCommand();
         $commands[] = new Command\Environment\EnvironmentInitCommand();
         $commands[] = new Command\Environment\EnvironmentMergeCommand();
+        $commands[] = new Command\Environment\EnvironmentPauseCommand();
         $commands[] = new Command\Environment\EnvironmentPushCommand();
         $commands[] = new Command\Environment\EnvironmentRedeployCommand();
         $commands[] = new Command\Environment\EnvironmentRelationshipsCommand();
+        $commands[] = new Command\Environment\EnvironmentResumeCommand();
         $commands[] = new Command\Environment\EnvironmentSshCommand();
         $commands[] = new Command\Environment\EnvironmentScpCommand();
         $commands[] = new Command\Environment\EnvironmentSynchronizeCommand();
