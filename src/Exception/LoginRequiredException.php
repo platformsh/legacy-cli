649868b748b68f945627eb411c9543654f1407ee
Patrick Dawkins <patrick@platform.sh>
2023-07-03 12:58:04 +0100

    Fix "Incorrectly nested style tag found" error
    Fixed by removing format tags from login required exception message.
    
    The "Incorrectly nested style tag" is caused by the naive rendering of the exception, which wraps text depending on the terminal width, and incorrectly wraps style tags, and incorrectly then tries to format all of that line by line, so for example `<comment>` can end up being formatted as `<comm` and then `ent>`.
    
    This would have been more recently revealed by https://github.com/platformsh/legacy-cli/commit/42dcd2048de4118213618d3caec65ce5bbc5590a because it adds a couple more style tags, making it more likely to happen.
    
    Fixes https://github.com/platformsh/cli/issues/85

--- src/Exception/LoginRequiredException.php
+++ src/Exception/LoginRequiredException.php
@@ -19,9 +19,9 @@ class LoginRequiredException extends HttpException
         $this->config = $config ?: new Config();
         $executable = $this->config->get('application.executable');
         $envPrefix = $this->config->get('application.env_prefix');
-        $message .= "\n\nPlease log in by running:\n    <comment>$executable login</comment>"
-            . "\n\nAlternatively, to log in using an API token (without a browser), run: <comment>$executable auth:api-token-login</comment>"
-            . "\n\nTo authenticate non-interactively, configure an API token using the <comment>{$envPrefix}TOKEN</comment> environment variable.";
+        $message .= "\n\nPlease log in by running:\n    $executable login"
+            . "\n\nAlternatively, to log in using an API token (without a browser), run: $executable auth:api-token-login"
+            . "\n\nTo authenticate non-interactively, configure an API token using the {$envPrefix}TOKEN environment variable.";
 
         parent::__construct($message, $previous);
     }
