0c9591f342eb51c7c2ce5d86bbb633f5e8cfbbe1
Patrick Dawkins <patrick@platform.sh>
2023-12-06 20:28:04 +0000

    Always create the lock file to avoid a race condition

--- src/Service/FileLock.php
+++ src/Service/FileLock.php
@@ -43,9 +43,6 @@ class FileLock
         $filename = $this->filename($lockName);
         $start = \time();
         while (\time() - $start < $this->timeLimit) {
-            if (!\file_exists($filename)) {
-                break;
-            }
             $content = $this->readWithLock($filename);
             $lockedAt = \intval($content);
             if ($lockedAt === 0 || \time() >= $lockedAt + $this->timeLimit) {
@@ -108,13 +105,18 @@ class FileLock
     }
 
     /**
-     * Reads a file using a shared lock.
+     * Reads a lock file.
+     *
+     * This uses a shared "flock" lock to synchronize reads.
+     *
+     * It creates the file if it does not already exist.
      *
      * @param string $filename
      * @return string
      */
     private function readWithLock($filename)
     {
+        $this->ensureExists($filename);
         $handle = \fopen($filename, 'r');
         if (!$handle) {
             throw new \RuntimeException('Failed to open file for reading: ' . $filename);
@@ -139,7 +141,10 @@ class FileLock
     }
 
     /**
-     * Writes to a file using an exclusive lock.
+     * Writes to a lock file.
+     *
+     * This uses a shared "flock" lock to synchronize writes and "fsync" or
+     * "fflush" to avoid buffering.
      *
      * @param string $filename
      * @param string $content
@@ -147,12 +152,7 @@ class FileLock
      */
     private function writeWithLock($filename, $content)
     {
-        $dir = \dirname($filename);
-        if (!\is_dir($dir)) {
-            if (!\mkdir($dir, 0777, true)) {
-                throw new \RuntimeException('Failed to create directory: ' . $dir);
-            }
-        }
+        (new \Symfony\Component\Filesystem\Filesystem())->mkdir(\dirname($filename));
         $handle = \fopen($filename, 'w');
         if (!$handle) {
             throw new \RuntimeException('Failed to open file for writing: ' . $filename);
@@ -164,17 +164,59 @@ class FileLock
             if (\fputs($handle, $content) === false) {
                 throw new \RuntimeException('Failed to write to file: ' . $filename);
             }
-            if (PHP_VERSION_ID >= 81000) {
-                if (!\fsync($handle)) {
-                    \trigger_error('Failed to sync file (fsync): ' . $filename, E_USER_WARNING);
-                }
-            } elseif (!\fflush($handle)) {
-                \trigger_error('Failed to flush file (fflush): ' . $filename, E_USER_WARNING);
-            }
+            $this->flushOrSync($handle, $filename);
         } finally {
             if (!\flock($handle, LOCK_UN)) {
                 \trigger_error('Failed to unlock file: ' . $filename, E_USER_WARNING);
             }
+            if (!\fclose($handle)) {
+                \trigger_error('Failed to close file: ' . $filename, E_USER_WARNING);
+            }
+        }
+    }
+
+    /**
+     * Syncs changes to a file using fsync() where available, otherwise fflush().
+     *
+     * @param resource $handle
+     * @param string $filename
+     * @return void
+     */
+    private function flushOrSync($handle, $filename)
+    {
+        if (PHP_VERSION_ID >= 81000) {
+            if (!\fsync($handle)) {
+                \trigger_error('Failed to sync file (fsync): ' . $filename, E_USER_WARNING);
+            }
+        } elseif (!\fflush($handle)) {
+            \trigger_error('Failed to flush file (fflush): ' . $filename, E_USER_WARNING);
+        }
+    }
+
+    /**
+     * Ensures a lock file exists without modifying its content.
+     *
+     * @param string $filename
+     * @return void
+     */
+    private function ensureExists($filename)
+    {
+        if (\file_exists($filename)) {
+            return;
+        }
+        (new \Symfony\Component\Filesystem\Filesystem())->mkdir(\dirname($filename));
+        if (\file_exists($filename)) {
+            // The file may have been created by another process by now.
+            return;
+        }
+        $handle = \fopen($filename, 'a');
+        if (!$handle) {
+            \trigger_error('Failed to create file: ' . $filename, E_USER_WARNING);
+            return;
+        }
+        try {
+            $this->flushOrSync($handle, $filename);
+        } finally {
             if (!\fclose($handle)) {
                 \trigger_error('Failed to close file: ' . $filename, E_USER_WARNING);
             }
