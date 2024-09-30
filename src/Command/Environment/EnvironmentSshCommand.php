9063275ddfc3bee36f97a0b3d34d3311c06f6ffe
Patrick Dawkins <pjcdawkins@users.noreply.github.com>
2024-09-30 10:15:41 +0100

    Explain SSH unavailability when an env is paused (#1485)

--- src/Command/Environment/EnvironmentSshCommand.php
+++ src/Command/Environment/EnvironmentSshCommand.php
@@ -3,6 +3,7 @@ namespace Platformsh\Cli\Command\Environment;
 
 use Platformsh\Cli\Command\CommandBase;
 use Platformsh\Cli\Service\Ssh;
+use Platformsh\Cli\Util\OsUtil;
 use Platformsh\Client\Exception\EnvironmentStateException;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
@@ -51,23 +52,32 @@ class EnvironmentSshCommand extends CommandBase
 
         try {
             $container = $this->selectRemoteContainer($input);
+            $sshUrl = $container->getSshUrl($input->getOption('instance'));
         } catch (EnvironmentStateException $e) {
-            if ($e->getEnvironment()->id === $environment->id && $e->getEnvironment()->status === 'inactive') {
-                $this->stdErr->writeln(sprintf('The environment %s is inactive, so an SSH connection is not possible.', $this->api()->getEnvironmentLabel($e->getEnvironment(), 'error')));
-                if (!$e->getEnvironment()->has_code) {
-                    $this->stdErr->writeln('');
-                    $this->stdErr->writeln('Push code to the environment to activate it.');
-                }
-                return 1;
+            if ($e->getEnvironment()->id !== $environment->id) {
+                throw $e;
+            }
+            switch ($e->getEnvironment()->status) {
+                case 'inactive':
+                    $this->stdErr->writeln(sprintf('The environment %s is inactive, so an SSH connection is not possible.', $this->api()->getEnvironmentLabel($e->getEnvironment(), 'error')));
+                    if (!$e->getEnvironment()->has_code) {
+                        $this->stdErr->writeln('');
+                        $this->stdErr->writeln('Push code to the environment to activate it.');
+                    }
+                    return 1;
+
+                case 'paused':
+                    $this->stdErr->writeln(sprintf('The environment %s is paused, so an SSH connection is not possible.', $this->api()->getEnvironmentLabel($e->getEnvironment(), 'error')));
+                    if ($this->config()->isCommandEnabled('environment:resume')) {
+                        $this->stdErr->writeln('');
+                        $this->stdErr->writeln(sprintf('Resume the environment by running: <info>%s resume -e %s</info>', $this->config()->get('application.executable'), OsUtil::escapeShellArg($environment->id)));
+                    }
+                    return 1;
             }
             throw $e;
-        }
-
-        try {
-            $sshUrl = $container->getSshUrl($input->getOption('instance'));
         } catch (\InvalidArgumentException $e) {
             // Use Symfony's exception to print usage information.
-            throw new InvalidArgumentException($e->getMessage());
+            throw $e instanceof InvalidArgumentException ? $e : new InvalidArgumentException($e->getMessage());
         }
 
         if ($input->getOption('pipe')) {
