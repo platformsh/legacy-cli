953ae9d96fbb27d004b1a8c776d86c06f4f97fa5
Patrick Dawkins <patrick@platform.sh>
2024-12-01 20:39:25 +0000

    Update HostFactory

--- src/Service/HostFactory.php
+++ src/Service/HostFactory.php
@@ -4,11 +4,12 @@ namespace Platformsh\Cli\Service;
 
 use Platformsh\Cli\Model\Host\LocalHost;
 use Platformsh\Cli\Model\Host\RemoteHost;
+use Platformsh\Client\Model\Environment;
 
 class HostFactory {
-    private $shell;
-    private $ssh;
-    private $sshDiagnostics;
+    private Shell $shell;
+    private Ssh $ssh;
+    private SshDiagnostics $sshDiagnostics;
 
     public function __construct(Shell $shell, Ssh $ssh, SshDiagnostics $sshDiagnostics) {
         $this->shell = $shell;
@@ -16,11 +17,11 @@ class HostFactory {
         $this->sshDiagnostics = $sshDiagnostics;
     }
 
-    public function local() {
+    public function local(): LocalHost {
         return new LocalHost($this->shell);
     }
 
-    public function remote($sshUrl) {
-        return new RemoteHost($sshUrl, $this->ssh, $this->shell, $this->sshDiagnostics);
+    public function remote(string $sshUrl, Environment $environment): RemoteHost {
+        return new RemoteHost($sshUrl, $environment, $this->ssh, $this->shell, $this->sshDiagnostics);
     }
 }
