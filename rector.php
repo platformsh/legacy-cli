16850817818c5bd70911d883574062eba0886e16
Patrick Dawkins <patrick@platform.sh>
2024-12-02 12:54:27 +0000

    Add new rector rule for DI and other sets

--- rector.php
+++ rector.php
@@ -2,18 +2,33 @@
 
 declare(strict_types=1);
 
+use Platformsh\Cli\Command\CommandBase;
+use Platformsh\Cli\Rector\InjectCommandServicesRector;
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Set\SymfonySetList;
+use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Symfony61\Rector\Class_\CommandConfigureToAttributeRector;
+use Rector\Transform\Rector\MethodCall\MethodCallToPropertyFetchRector;
+use Rector\Transform\ValueObject\MethodCallToPropertyFetch;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    ->withSets([
-        SymfonySetList::SYMFONY_71,
-    ])
     ->withRules([
         CommandConfigureToAttributeRector::class,
-    ]);
+        InjectCommandServicesRector::class,
+    ])
+    ->withImportNames(importShortClasses: false, removeUnusedImports: true)
+    ->withConfiguredRule(MethodCallToPropertyFetchRector::class,
+        [new MethodCallToPropertyFetch(CommandBase::class, 'api', 'api')])
+    ->withConfiguredRule(MethodCallToPropertyFetchRector::class,
+        [new MethodCallToPropertyFetch(CommandBase::class, 'config', 'config')])
+    ->withSets([
+        SetList::PHP_74,
+        SetList::PHP_80,
+        SetList::PHP_81,
+        SetList::PHP_82,
+        SetList::TYPE_DECLARATION,
+    ])
+    ;
