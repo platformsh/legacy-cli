10c6d65665665c1542f2e93eea8a95c70fccd9de
Patrick Dawkins <patrick@platform.sh>
2024-12-15 11:03:58 +0000

    Add golangci-lint for Go tests

--- go-tests/org_list_test.go
+++ go-tests/org_list_test.go
@@ -1,36 +1,36 @@
 package tests
 
 import (
-    "net/http/httptest"
-    "net/url"
-    "strings"
-    "testing"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 
-    "github.com/platformsh/cli/pkg/mockapi"
+	"github.com/platformsh/cli/pkg/mockapi"
 )
 
 func TestOrgList(t *testing.T) {
-    authServer := mockapi.NewAuthServer(t)
-    defer authServer.Close()
+	authServer := mockapi.NewAuthServer(t)
+	defer authServer.Close()
 
-    myUserID := "user-id-1"
+	myUserID := "user-id-1"
 
-    apiHandler := mockapi.NewHandler(t)
-    apiHandler.SetMyUser(&mockapi.User{ID: myUserID})
-    apiHandler.SetOrgs([]*mockapi.Org{
-        makeOrg("org-id-1", "acme", "ACME Inc.", myUserID),
-        makeOrg("org-id-2", "four-seasons", "Four Seasons Total Landscaping", myUserID),
-        makeOrg("org-id-3", "duff", "Duff Beer", "user-id-2"),
-    })
+	apiHandler := mockapi.NewHandler(t)
+	apiHandler.SetMyUser(&mockapi.User{ID: myUserID})
+	apiHandler.SetOrgs([]*mockapi.Org{
+		makeOrg("org-id-1", "acme", "ACME Inc.", myUserID),
+		makeOrg("org-id-2", "four-seasons", "Four Seasons Total Landscaping", myUserID),
+		makeOrg("org-id-3", "duff", "Duff Beer", "user-id-2"),
+	})
 
-    apiServer := httptest.NewServer(apiHandler)
-    defer apiServer.Close()
+	apiServer := httptest.NewServer(apiHandler)
+	defer apiServer.Close()
 
-    run := runnerWithAuth(t, apiServer.URL, authServer.URL)
+	run := runnerWithAuth(t, apiServer.URL, authServer.URL)
 
-    assert.Equal(t, strings.TrimLeft(`
+	assert.Equal(t, strings.TrimLeft(`
 +--------------+--------------------------------+-----------------------+
 | Name         | Label                          | Owner email           |
 +--------------+--------------------------------+-----------------------+
@@ -40,14 +40,14 @@ func TestOrgList(t *testing.T) {
 +--------------+--------------------------------+-----------------------+
 `, "\n"), run("orgs"))
 
-    assert.Equal(t, strings.TrimLeft(`
+	assert.Equal(t, strings.TrimLeft(`
 Name	Label	Owner email
 acme	ACME Inc.	user-id-1@example.com
 duff	Duff Beer	user-id-2@example.com
 four-seasons	Four Seasons Total Landscaping	user-id-1@example.com
 `, "\n"), run("orgs", "--format", "plain"))
 
-    assert.Equal(t, strings.TrimLeft(`
+	assert.Equal(t, strings.TrimLeft(`
 org-id-1,acme
 org-id-3,duff
 org-id-2,four-seasons
@@ -55,12 +55,12 @@ org-id-2,four-seasons
 }
 
 func makeOrg(id, name, label, owner string) *mockapi.Org {
-    return &mockapi.Org{
-        ID:           id,
-        Name:         name,
-        Label:        label,
-        Owner:        owner,
-        Capabilities: []string{},
-        Links:        mockapi.MakeHALLinks("self=/organizations/" + url.PathEscape(id)),
-    }
+	return &mockapi.Org{
+		ID:           id,
+		Name:         name,
+		Label:        label,
+		Owner:        owner,
+		Capabilities: []string{},
+		Links:        mockapi.MakeHALLinks("self=/organizations/" + url.PathEscape(id)),
+	}
 }
