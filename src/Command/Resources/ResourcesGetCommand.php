5b9701d581fff642f69fd398c6132ff22ee5af70
Ricardo Kirkner <ricardokirkner@gmail.com>
2025-08-29 13:27:02 -0300

    Include a badge next to the service name when autoscaling is enabled (#1557)

--- src/Command/Resources/ResourcesGetCommand.php
+++ src/Command/Resources/ResourcesGetCommand.php
@@ -74,6 +74,13 @@ class ResourcesGetCommand extends ResourcesCommandBase
             return 1;
         }
 
+        // Check autoscaling settings for the environment, as autoscaling prevents changing some resources manually.
+        $autoscalingSettings = $this->api()->getAutoscalingSettings($environment)->getData();
+        $autoscalingEnabled = [];
+        foreach ($autoscalingSettings['services'] as $service => $serviceSettings) {
+            $autoscalingEnabled[$service] = !empty($serviceSettings['enabled']);
+        }
+
         /** @var Table $table */
         $table = $this->getService('table');
 
@@ -91,8 +98,14 @@ class ResourcesGetCommand extends ResourcesCommandBase
 
         $rows = [];
         $cpuTypeOption = $input->getOption('cpu-type');
+        $autoscalingIndicator = '<comment>(A)</comment>';
+        $hasAutoscalingIndicator = false;
         foreach ($services as $name => $service) {
             $properties = $service->getProperties();
+            if (!$table->formatIsMachineReadable() && !empty($autoscalingEnabled[$name])) {
+                $name .= ' ' . $autoscalingIndicator;
+                $hasAutoscalingIndicator = true;
+            }
             $row = [
                 'service' => $name,
                 'type' => $formatter->format($service->type, 'service_type'),
@@ -145,6 +158,10 @@ class ResourcesGetCommand extends ResourcesCommandBase
         $table->render($rows, $this->tableHeader, $this->defaultColumns);
 
         if (!$table->formatIsMachineReadable()) {
+            if ($hasAutoscalingIndicator) {
+                $this->stdErr->writeln($autoscalingIndicator . ' - Indicates that the service has autoscaling enabled');
+            }
+
             $executable = $this->config()->get('application.executable');
             $isOriginalCommand = $input instanceof ArgvInput;
             if ($isOriginalCommand) {
